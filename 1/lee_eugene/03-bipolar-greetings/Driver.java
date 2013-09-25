41b0de492f7f060eead7f8c26e00bbeec4514daf
Eugene Lee <eugenelee@users.noreply.github.com>
2013-09-25 17:34:29 -0400

    Fixed a couple of derpy mistakes here and there.

--- 1/lee_eugene/03-bipolar-greetings/Driver.java
+++ 1/lee_eugene/03-bipolar-greetings/Driver.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 
 public class Driver {
-	public class void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Testing out constructor Greeter() on Mr. Z.");
 		Greeter g1 = new Greeter();
-		g1.happyGreet("Mr. Zamansky");
-		g1.sadGreet("Mr. Zamansky");
+		System.out.println(g1.happyGreet("Mr. Zamansky"));
+		System.out.println(g1.sadGreet("Mr. Zamansky"));
 		
 		System.out.println("Testing out constructor Greeter(h,s) on Mr. Z.");
 		Greeter g2 = new Greeter("Hey, it's","Bah it's");
-		g2.happyGreet("Mr. Zamansky");
-		g2.sadGreet("Mr. Zamansky");
+		System.out.println(g2.happyGreet("Mr. Zamansky"));
+		System.out.println(g2.sadGreet("Mr. Zamansky"));
 	}
 }
