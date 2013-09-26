ac74b3bcd6cf25e82c37ce458354dbc79e85a1bc
Victor Wu <vicwu97@gmail.com>
2013-09-26 08:35:50 -0400

    Turtles

---
+++ 1/wu_victor/4-Speed/Turtle.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+Public class Turtle {
+    
+    Private String Turtle;
+    Private int age;
+    Private int speed;
+
+    Public Turtle () {
+	age = 2
+    }
+
+    Public Turtle (String name) {
+
+    }
+
+    Public Turtle (String name, int age, int speed ) {
+
+    }
+
+
+}
+ 
+
