35cc3569e1cc7636d171d61a931cba8e495c62a7
tdiep26 <timd33@gmail.com>
2013-09-25 22:18:15 -0400

    Added Happy and Sad greeters

--- 2/kan_kevin/2-greeter/Driver.java
+++ 2/kan_kevin/2-greeter/Driver.java
@@ -5,21 +5,14 @@ public class Driver {
     public static void main(String[] args) {
 	Greeter g,g2;
 	String result;
-	g = new Greeter();
-	g.setGreeting("Hello");
-	g2 = new Greeter();
-	g2.setGreeting("How are you doing");
+	g = new Greeter("Welcome");
+	//	g.setHappyGreeting("Welcome");
+	g2 = new Greeter("Go away");
+	//	g2.setSadGreeting("Go away");
 	result = g.greet("Tom");
 	System.out.println(result);
 	result = g2.greet("Sarah");
 	System.out.println(result);
-
-	/* 1. Try:
-	      g.setGreeting("Howdy");
-	         g.greet("Tom");
-		    g.setGreeting("sup");
-		       g.greet("Fred");
-	*/
 
     }
 }
