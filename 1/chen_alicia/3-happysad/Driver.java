717f7e736116e1fe604caa36389f5d7aa4adb57b
tdiep26 <timd33@gmail.com>
2013-09-25 23:04:51 -0400

    Added Happy and Sad greeters

--- 1/chen_alicia/3-happysad/Driver.java
+++ 1/chen_alicia/3-happysad/Driver.java
@@ -3,15 +3,11 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args) {
+
 	Greeter g;
 	String result;
 
-	g = new Greeter("Welcome","Begone");
-
-	System.out.println(g.happyGreet("Jeff"));
-	System.out.println(g.sadGreet("Barry"));
-
-
-	
+	g = new Greeter("such a lovely day,", "go away");
+	System.out.println(g.greet("Ishmam"));
     }
 }
--- 3/diep_timothy/3-git-pt-2/Greeter.java
+++ 3/diep_timothy/3-git-pt-2/Greeter.java
@@ -3,21 +3,9 @@ import java.util.*;
 
 public class Greeter {
 
-    /* We can initialize an instance variable
-       when we declare it like this:
-    */
-    //private String greeting="Go away"; 
-
-
-    private String greeting;
     public String happygreeting,sadgreeting; 
 
-    public Greeter(String g) {
-	setGreeting(g);
-    }
-
     public Greeter() {
-	setGreeting("Hello");
 	happygreeting = "Welcome";
 	sadgreeting = "Begone";
     }	
@@ -27,22 +15,12 @@ public class Greeter {
 	setSad(sadgreeting);
     }
 
-    public void setGreeting(String greet) {
-	greeting = greet;
-    }
-
     public void setHappy(String happygreet) {
 	happygreeting = happygreet;
     }
 
     public void setSad(String sadgreet) {
 	sadgreeting = sadgreet;
-    }
-
-    public String greet(String name) {
-	String message;
-	message = greeting + " " + name;
-	return message;
     }
 
     public String happyGreet(String name) {
@@ -56,34 +34,6 @@ public class Greeter {
 	message = sadgreeting + " " + name;
 	return message;
     }
-
-
-    /* 
-       Examples of method overloading. 
-       Notice that each method has a
-       unique signature, that is, the order and types of the
-       parameters are unique. 
-       Java doesn't care about the parameter names
-    */
-    public void f() {
-    }
-    public void f(String name1)  {
-    }
-    public void f(String name1, String name2) {
-    }
-    public void f(String name1, int name2) {
-    }
-    public void f(int name1, String name) {
-    }
-    /*
-      This one doesn't work because we already
-      have a method f with a String, String 
-      signature
-
-    public void f(String name2, String name1) {
-    }
-    */
-
 
 }
 
