1c9d5233bbebb8cc41e6c10be2536e06a9c61dde
Sungwoo Park <iyw03211@gmail.com>
2013-09-27 09:49:06 -0400

    Created Driver.java and Turtle.java

---
+++ 4-turtle/Turtle.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Turtle(){
+	private String name;
+	private int age;
+	private int speed;
+
+	public Turtle(){
+		setName("sadhjksahdsad");
+		setAge(1231124);
+		setSpeed(2);
+	}
+	
+	public Turtle(String name){
+		
+	}
+
+	public Turtle(String name, int age, int speed){
+
+	}
+
+	public void setName(String n){
+		name = n;		
+	}
+
+	public void setAge(int a){
+		age = a;
+	}
+
+	public void setSpeed(int s){
+		speed = s;
+	}
+}
+
