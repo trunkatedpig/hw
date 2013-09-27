458b0e53d01279f62195dab12f086b08a67274bf
Sungwoo Park <iyw03211@gmail.com>
2013-09-27 10:15:48 -0400

    finished some code

--- 4-turtle/Turtle.java
+++ 4-turtle/Turtle.java
@@ -3,22 +3,30 @@ import java.util.*;
 
 public class Turtle{
 	private String name;
-	private int age;
-	private int speed;
+	private int age = 1;
+	private int speed = 2;
 
 	public Turtle(){
-		setName("sadhjksahdsad");
-		setAge(1231124);
-		setSpeed(2);
+	    setName("Freddie");
 	}
 	
 	public Turtle(String name){
-		
+	    setName(name);
 	}
 
 	public Turtle(String name, int age, int speed){
-
+	    setName(name);
+	    setAge(age);
+	    setSpeed(speed);
 	}
+
+    public void speedUp(int n){
+	setSpeed(speed + n);
+    }
+
+    public void slowDown(int n){
+	setSpeed(speed - n);
+    }
 
 	public void setName(String n){
 		name = n;		
