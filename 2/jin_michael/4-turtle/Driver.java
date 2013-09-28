811aa5dc205e0a9dfc33cd6704bc5b57c7b1adbe
herowatch <michael.jin123@gmail.com>
2013-09-28 19:51:05 -0400

    adding 4-turtle 2

--- 2/jin_michael/4-turtle/Driver.java
+++ 2/jin_michael/4-turtle/Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
     public static void main(String args[]){
 	Turtle first;
-	first = new Turtle("Bob3",20,0);
+	first = new Turtle();
 	System.out.println(first.getName());
 	System.out.println(first.getSpeed());
 	System.out.println(first.getAge());
