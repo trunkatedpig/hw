b14b14f3103983ed1231f2e90e55b1ddf9a6eaf7
CobyGoldbergs <CobyGoldbergs@users.noreply.github.com>
2013-09-29 09:54:48 -0400

    Part 1 working

--- 1/goldberg_jacob/turtlework/Turtle.java
+++ 1/goldberg_jacob/turtlework/Turtle.java
@@ -1,17 +1,28 @@
 public class Turtle {
     private String turtle;
     private int speed,age;
+
+    public void setturtle(String n){
+	turtle = n;
+    }
+    public void setage(int m){
+	age = m;
+    }
+    public void setspeed(int s){
+	speed = s;
+    }
+
     public Turtle() {
-	speed = 0;
-	age = 0;
-	turtle = "Tom";
+	setspeed(10);
+	setage(93);
+	setturtle("Tom");
     }
     public Turtle(String name){
-	turtle = name;
-	age = 0;
-	speed = 0;
+	setturtle(name);
+	setage(7);
+	setspeed(11);
     }
-    public void SpeedUp(int n){
+    /*public void SpeedUp(int n){
 	setspeed(speed + n);
     }
     public void SlowDown(int n){
@@ -19,7 +30,15 @@ public class Turtle {
     }
     public void GetOlder(){
 	setage(age + 1) 
+    }*/
+
+    public String tellname(){
+	return turtle;
     }
-
-
- }
+    public int tellage(){
+	return age;
+    }
+    public int tellspeed(){
+	return speed;
+    }
+}
