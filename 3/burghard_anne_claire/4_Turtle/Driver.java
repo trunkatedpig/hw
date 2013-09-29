856e2012278488dbdbd2f1f340b42f3d6280b494
Claire Burghard <claireburghard@gmail.com>
2013-09-29 16:07:22 -0400

    Revert "Turtles completed"
    This reverts commit 1da5335de448a75cefe4e6a077683d146d2d39c5.

--- 3/burghard_anne_claire/4_Turtle/Driver.java
+++ 3/burghard_anne_claire/4_Turtle/Driver.java
@@ -1,21 +1,16 @@
 import java.io.*;
 import java.util.*;
+
 public class Driver{
     public static void main(String[] args) {
 	Turtle t,t2,t3;
-	Person p;
-
 	t = new Turtle();
-	p= new Person("Harry");
-	p.setTurtle(t);
 	System.out.println(t.getName());
 	System.out.println(t.getAge());
 	System.out.println(t.getOlder());
 	System.out.println(t.getSpeed());
 	System.out.println(t.speedUp(1));
 	System.out.println(t.slowDown(1));
-	System.out.println("turtle speed:" + p.getTurtleSpeed());
-	System.out.println("My turtle:" + p.getPet());
 
 	t2 = new Turtle("Claire");
 	System.out.println(t2.getName());
@@ -32,6 +27,9 @@ public class Driver{
 	System.out.println(t3.getSpeed());
   	System.out.println(t3.speedUp(1));
 	System.out.println(t3.slowDown(1));
+
     }
 }
+
+
 
