693514aa0cfb55c0764319495a7d04397c23994c
Claire Burghard <claireburghard@gmail.com>
2013-09-29 12:58:39 -0400

    Turtles part 1

---
+++ 3/burghard_anne_claire/4_Turtle/Driver.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+    public static void main(String[] args) {
+	Turtle t,t2,t3;
+	t = new Turtle();
+	System.out.println(t.getName());
+	System.out.println(t.getAge());
+	System.out.println(t.getOlder());
+	System.out.println(t.getSpeed());
+	System.out.println(t.speedUp(1));
+	System.out.println(t.slowDown(1));
+
+	t2 = new Turtle("Claire");
+	System.out.println(t2.getName());
+	System.out.println(t2.getAge());
+	System.out.println(t2.getOlder());
+	System.out.println(t2.getSpeed());
+	System.out.println(t2.speedUp(1));
+	System.out.println(t2.slowDown(1));
+	
+	t3 = new Turtle("Sydney", 45, 19);
+	System.out.println(t3.getName());
+	System.out.println(t3.getAge());
+	System.out.println(t3.getOlder());
+	System.out.println(t3.getSpeed());
+  	System.out.println(t3.speedUp(1));
+	System.out.println(t3.slowDown(1));
+
+    }
+}
+
+
+
