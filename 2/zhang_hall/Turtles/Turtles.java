5b97b7aac3275ea9f8d465c4d6929964d9bba1d4
Hall <the1zhang@gmail.com>
2013-09-29 19:41:35 -0400

    Addedall constructors

--- 2/zhang_hall/Turtles/Turtles.java
+++ 2/zhang_hall/Turtles/Turtles.java
@@ -2,11 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Turtles {
-
+    //Instance Variables;
     private String name;
     private int age,speed;
 
+    //Constructor with no parameters;
+    public void Turtle (){
+	name = "Udyr";
+	age = 20;
+	speed = 10;
+    }
     
+    //Constructor with name parameter;
+    public void Turtle (String n){
+	name = n;
+	age = 25;
+	speed = 30;
+    }
+    
+    //Constructor with all three accepted parameters
+    public void Turtle (String n, int a, int s){
+	name = n;
+	speed = s;
+	age = a;
+    }
+	
+    //methods;
     public void speedUp(int n){
 	speed = speed + n;
     }
@@ -15,6 +36,7 @@ public class Turtles {
 	speed = speed - n;
     }
 
+    //getmethods;
     public void getOlder(){
 	age = age + 1;
     }
