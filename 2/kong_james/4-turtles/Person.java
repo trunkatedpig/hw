a50921b47b86f56ff6a4438c48e0ad9316fc3cf6
jameskong <jdetect1@gmail.com>
2013-09-29 14:53:02 -0400

    not complete

---
+++ 2/kong_james/4-turtles/Person.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Person {
+    public String name;
+    public Turtle;
+ 
+    public Person(String n){
+	name = n;
+    }
+    public void setTurtle(turtle t){
+	Turtle = t;
+    }
+    public turtle getPet(){
+	return t;
+    }
+    public int getTurtleSpeed(){
+	return speed;
+    }
+}  
