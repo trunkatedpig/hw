1da5335de448a75cefe4e6a077683d146d2d39c5
Claire Burghard <claireburghard@gmail.com>
2013-09-29 16:04:54 -0400

    Turtles completed

---
+++ 3/burghard_anne_claire/Turtle.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Turle{
+    private String name;
+    private int age,speed;
+
+    public void speedUp(int n){
+	return speed + n
+	    }
+  
