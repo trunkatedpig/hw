9a3818e0b1030b085774725cb067c5cc2727d2cd
jameskong <jdetect1@gmail.com>
2013-09-29 16:20:53 -0400

    Finished HW4

--- 2/kong_james/4-turtles/Driver.java
+++ 2/kong_james/4-turtles/Driver.java
@@ -6,5 +6,11 @@ public class Driver {
     public static void main (String [] args) {
 	Person p;
 	p = new Person("Bob");
+	Turtle t;
+	t = new Turtle("Bobby",1,2);
+	System.out.println("Turtle name: " + t.getName());
+	System.out.println("Turtle speed: " + t.getSpeed());
+	System.out.println("turtle age:" + t.getAge());
+	
     }
 }
