407a7b717f48dce60832e0b07c558c34c03f5719
KbhackerVGM97 <kbsobhai@gmail.com>
2013-09-29 21:27:52 -0400

    fixed constructors

--- 1/sobhai_kemraj/4-Turtle/Driver.java
+++ 1/sobhai_kemraj/4-Turtle/Driver.java
@@ -6,16 +6,14 @@ public class Driver {
 	//Turtle Class Tests   
 	Turtle one; 
 	one = new Turtle();
-	one.setTurtle ();
 	one.speedUp(10);
 	System.out.println("Turtle one's name is " + one.getname());
 	System.out.println(one.getname() +  " is "  + one.getage() +
 			   " years old.");
 	System.out.println(one.getname() + "'s speed is " + one.getspeed()); 
-		
+
 	Turtle two;
-	two = new Turtle();
-	two.setTurtleName ("Kemraj");
+	two = new Turtle("Kemraj");
 	two.slowDown(2);
 	System.out.println("Turtle two's name is " + two.getname());
 	System.out.println(two.getname() + " is " + two.getage()+ 
@@ -23,13 +21,17 @@ public class Driver {
 	System.out.println(two.getname() + "'s speed is " + two.getspeed());
         
 	Turtle three;
-	three = new Turtle();
-	three.setTurtleNameInfo ("Alicia",20,16);
+	three = new Turtle("Alicia",20,16);
 	three.getOlder();
 	System.out.println("Turtle three's name is " + three.getname());
 	System.out.println(three.getname() + " is " + three.getage() +
 			   " years old.");
 	System.out.println(three.getname() + "'s speed is " + 
 			   three.getspeed());
+
+	//Person testing
+	Person bob;
+	bob = Person ("Robert")
+	
     }
 }	
