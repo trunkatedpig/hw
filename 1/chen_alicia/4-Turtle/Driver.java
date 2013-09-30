fa8cdd34dd1242055395b7bd5e3664e65757fa13
aliciachen <mail4aliciac@gmail.com>
2013-09-29 21:15:40 -0400

    Changes to Driver

--- 1/chen_alicia/4-Turtle/Driver.java
+++ 1/chen_alicia/4-Turtle/Driver.java
@@ -29,3 +29,38 @@ public class Driver {
 	System.out.println(three.getspeed());
     }
 }	
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	//Turtle Class Tests   
+	Turtle one; 
+	one = new Turtle();
+	one.setTurtle ();
+	one.speedUp(10);
+	System.out.println("Turtle one's name is " + one.getname());
+	System.out.println(one.getname() +  " is "  + one.getage() +
+			   " years old.");
+	System.out.println(one.getname() + "'s speed is " + one.getspeed()); 
+
+	Turtle two;
+	two = new Turtle();
+	two.setTurtleName ("Kemraj");
+	two.slowDown(2);
+	System.out.println("Turtle two's name is " + two.getname());
+	System.out.println(two.getname() + " is " + two.getage()+ 
+			   " years old.");
+	System.out.println(two.getname() + "'s speed is " + two.getspeed());
+        
+	Turtle three;
+	three = new Turtle();
+	three.setTurtleNameInfo ("Alicia",20,16);
+	three.getOlder();
+	System.out.println("Turtle three's name is " + three.getname());
+	System.out.println(three.getname() + " is " + three.getage() +
+			   " years old.");
+	System.out.println(three.getname() + "'s speed is " + 
+			   three.getspeed());
+    }
+}	
