993d3a0c8aefeb659bde6fab839c7fa40a3809c8
BrianGuo <brianguosh@gmail.com>
2013-09-30 21:29:25 -0400

    "Added Coin.java"

---
+++ 1/guo_brian/5-Coin/Coin.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Coin{
+
+    public String Face;
+    public static int flips, percentage, heads, tails;
+
+    public void reset() {
+	flips = 0;
+	percentage = 0;
+	heads = 0;
+	tails = 0;
+    }
+
+    public string face(String a) {
+	face = a;
+    }
