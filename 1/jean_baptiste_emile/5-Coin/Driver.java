ed06f4afa2d08110bf6a56d7f4de2a76de0b45f4
Emile Jean-Baptiste <dr.darklord1228@gmail.com>
2013-10-01 06:09:32 -0400

    Added HW 5

---
+++ 1/jean_baptiste_emile/5-Coin/Driver.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+
+	Coin c;
+	c = new Coin();
+	System.out.println(c.getFace());
+	System.out.println(c.getFlips());
+	c.reset();
+	System.out.println(c.getFlips());
+    }
+}
