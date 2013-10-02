06ab547bd4aa3b984cbb37de0e66467888aac894
IsaacGluck <isaacbgluck@gmail.com>
2013-10-02 17:00:32 -0400

    redid initiation and constructors

--- 3/gluck_isaac/5-Coin/Coin.java
+++ 3/gluck_isaac/5-Coin/Coin.java
@@ -3,44 +3,47 @@ import java.io.*;
 
 public class Coin {
     public String face;
-    public int count;
-    public int flips;
-    public int prob;
+    public int tosses;
+    public int heads;
+    public double prob;
 
     
     public Coin() {
-	setCount(0);
-	setFlips(0);
-	prob = 0.5;
+        initCoin("Heads", 0.5);
     }
     
-
-    public void setCount(int n) {
-	count = n;
+    public void initCoin(String f, double p) {
+	setFace(f);
+	tosses = 0;
+	heads = 0;
+	setProb(p);
+    }
+    public void setFace(String f) {
+	face = f;
     }
 
-    public void setFlips(int n) {
-	count = n;
+    public void setProb(double p) {
+	prob = p;
     }
 
 
     public void resetCount() {
-	setCount(0);
+	tosses = 0;
     }
 
     public String getFace() {
 	return face;
     }
-
+    /*
     public String flip() {
 	Random r = new Random();
 	int a = r.nextint(2);
 	if (a == 1)
-	    setCount(count +1);
+	    setTosses(tosses +1);
 	////////////
 	    return "Head";
 	else
 	    return "Tail";
-	
-    }
+	    }
+    */
 }
