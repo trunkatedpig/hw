27f0d14a388658dcfd6c2baf0d206f3843a9a83a
ezlotnik <elisez97@gmail.com>
2013-10-02 18:40:17 -0400

    readded accidently deleted hw

---
+++ 1/zlotnikova_elise/5-coin/Driver.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args){ 
+	Coin Penny;
+	face = "heads"
+	flips = 6;
+	heads = 3;
+	tails = 3;
+	fairness = .50
+	Penny = new Coin(); 
+	
+	System.out.print("Face: "+face);
+
+
+    }
+}
