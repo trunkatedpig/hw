a546311be3b1e8b15b0d0d3dab299a54f67243fa
IsaacGluck <isaacbgluck@gmail.com>
2013-10-02 16:46:38 -0400

    in class

--- 3/gluck_isaac/5-Coin/Coin.java
+++ 3/gluck_isaac/5-Coin/Coin.java
@@ -1,17 +1,28 @@
+import java.util.*;
+import java.io.*;
+
 public class Coin {
     public String face;
     public int count;
+    public int flips;
     public int prob;
 
-    /*
+    
     public Coin() {
-	
+	setCount(0);
+	setFlips(0);
+	prob = 0.5;
     }
-    */
+    
 
     public void setCount(int n) {
 	count = n;
     }
+
+    public void setFlips(int n) {
+	count = n;
+    }
+
 
     public void resetCount() {
 	setCount(0);
@@ -19,5 +30,17 @@ public class Coin {
 
     public String getFace() {
 	return face;
+    }
+
+    public String flip() {
+	Random r = new Random();
+	int a = r.nextint(2);
+	if (a == 1)
+	    setCount(count +1);
+	////////////
+	    return "Head";
+	else
+	    return "Tail";
+	
     }
 }
