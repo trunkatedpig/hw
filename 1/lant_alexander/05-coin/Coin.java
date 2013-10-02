7dbb81b6318e2219bd87673d005e83813da81da7
Caspar Lant <casparlant@gmail.com>
2013-10-02 00:23:57 -0400

    Halfway there with Coin.java

--- 1/lant_alexander/05-coin/Coin.java
+++ 1/lant_alexander/05-coin/Coin.java
@@ -5,6 +5,15 @@ import java.io.*;
 	public static void main(String[] args) {
 
 	    Random r = new Random();
-	    System.out.println(r.nextInt());
+	    int r1 = r.nextInt(2);
+	
+	       if (r1 == 1) {
+		System.out.println("heads");
+	    } 
+	     else {
+	    	System.out.println("tails"); }
+		 
 	}
     }
+    
+
