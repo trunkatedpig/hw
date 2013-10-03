06d0ed09a3dba83d77832fe14e995480474213e9
Aaron <aaron@ubuntu>
2013-10-02 21:07:12 -0400

    Updated coin

--- 3/mortenson_aaron/5-coin/Coin.java
+++ 3/mortenson_aaron/5-coin/Coin.java
@@ -10,24 +10,33 @@ public class Coin {
 		fair = 50;
 		head = 0;
 		tail = 0;
+		face = true;
 	}
 
 	public Coin(int i) {
 		fair = i;
 		head = 0;
 		tail = 0;
+		face = true;
 	}
 
-	public String flip() {
+	public void flip() {
 		Random r = new Random();
 		if (fair > r.nextInt(100)) {
+			face = true;
 			head = head + 1;
-			return "Heads";
 		}
 		else {
+			face = false;
 			tail = tail + 1;
-			return "Tails";
 		}
+	}
+
+	public String getFace() {
+		if (face == true) 
+			return "Heads";
+		else
+			return "Tails";
 	}
 	
 	public int getHeads() {
