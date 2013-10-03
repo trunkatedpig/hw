b5bb16997ea6326727da25f19eff92f36d4dc77c
Spencer Weiss <spencerc.weiss@gmail.com>
2013-10-03 08:42:34 -0400

    Caspar rules

--- 1/weiss_spencer/5-coin/driver.java
+++ 1/weiss_spencer/5-coin/driver.java
@@ -5,5 +5,8 @@ public class driver {
     public static void main (String[] args) {
         coin c;
         c = new coin();
+	System.out.println(c);
+	f = new fairness();
+	System.out.println(c);
     }
 }
