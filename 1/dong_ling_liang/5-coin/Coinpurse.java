ba1d8491ed6f4db4e86dec407ff24810ccf52d59
Ling <lingd1944@gmail.com>
2013-10-03 22:26:41 -0400

    Update Coinpurse.java

--- 1/dong_ling_liang/5-coin/Coinpurse.java
+++ 1/dong_ling_liang/5-coin/Coinpurse.java
@@ -6,15 +6,15 @@ public class Coinpurse{
     private double amount;
     
     public Coinpurse(){
-    	amount = 0
+    	amount = 0;
     }
     
     public void addTo (Coin c) {
-	amount = Double.valueOf((amount.doubleValue()) + ((c.getAmount).doubleValue()))
+	amount = Double.valueOf((amount.doubleValue()) + ((c.getAmount).doubleValue()));
     }
     
     public void removeFrom (Coin c) {
-	amount = Double.valueOf((amount.doubleValue()) - ((c.getAmount).doubleValue()))
+	amount = Double.valueOf((amount.doubleValue()) - ((c.getAmount).doubleValue()));
     }
     public double purseValue(){
     	return amount;
