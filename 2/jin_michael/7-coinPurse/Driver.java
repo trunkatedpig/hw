7a9666871204d9a8c2595d1d2f35775d5a45d21d
herowatch <michael.jin123@gmail.com>
2013-10-03 20:15:10 -0400

    Adding coinPurse

--- 2/jin_michael/7-coinPurse/Driver.java
+++ 2/jin_michael/7-coinPurse/Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String args[]){
-	Coin first;
+	/*	Coin first;
 	first=new Coin();
 	first.resetCount();
 	System.out.println(first.getFace());
@@ -8,8 +8,16 @@ public class Driver{
 	System.out.println(first.getFace());
 	System.out.println(first.headsN());
 	System.out.println(first.tailsN());
-
-
+	*/
+	Coin coin;
+	coin=new Coin("quartesr");
+	CoinPurse purse;
+	purse= new CoinPurse();
+	purse.addTo(coin);
+	purse.removeFrom(coin);
+	purse.removeFrom(coin);
+	purse.removeFrom(coin);
+	System.out.println(purse.amount);
     }
 
 
