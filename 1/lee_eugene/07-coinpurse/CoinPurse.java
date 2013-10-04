a64052d10827d44cf263bb134480a9cf0575bc30
Eugene Lee <eugenelee@users.noreply.github.com>
2013-10-04 10:36:50 -0400

    Added initial version of coin purse.

---
+++ 1/lee_eugene/07-coinpurse/CoinPurse.java
@@ -0,0 +1,7 @@
+public class CoinPurse {
+    // Not so sure about CoinPurse - are you asking for an array of sorts?
+    public void addTo(Coin c){
+    }
+    public void removeFrom(Coin c) {
+    }
+}
