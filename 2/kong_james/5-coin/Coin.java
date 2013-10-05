1d31c2366bacfc2543387b37a3a63c54a4af705d
James Kong <jdetect1@gmail.com>
2013-10-04 23:27:31 -0400

    Uploaded from School Comp

--- 2/kong_james/5-coin/Coin.java
+++ 2/kong_james/5-coin/Coin.java
@@ -1,14 +1,29 @@
 import java.io.*;
 import java.util.*;
+//import Math.random.*;
 
 public class Coin {
     private String face;
     private int hcount;
     private int tcount;
-    private int fairness;
+    private double fairness = 0.5;
+    private int coin;
+    private int Random;
+    private int quarter = 25;
+    private int dime = 10;
+    private int nickel = 5;
+    private int penny = 1;
+
+    
+    public Coin(String f) {
+	face = f;
+	hcount = 0;
+	tcount = 0;
+	fairness = 0.5;
+    }
 
     public void perctHead(){
-	fairness = hcount / (hcount + tcount);
+	Random = nextDouble();
     }
   
     public void reset(){
@@ -19,4 +34,10 @@ public class Coin {
     public String getFace(){
 	return face;
     }
+    public Boolean flip(){
+	if ( Random > fairness){
+	    face = "head";}
+	else {face = "tails";}
+    }
+
 }
