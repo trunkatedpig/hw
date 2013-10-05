d41eb6c38d058a7f7802f607003537b159aff575
Jane Chen <jchen010197@gmail.com>
2013-10-05 18:21:26 -0400

    Fairness fixed

--- 2/chen_jane/5-coin/Coin.java
+++ 2/chen_jane/5-coin/Coin.java
@@ -4,42 +4,47 @@ import java.util.*;
 public class Coin {
 
     private String face;
-    private int flips,heads;
-    private int fairness;
+    private int flips,heads,tails;
+    private double fairness;
 
     public void resetCount() {
 	flips = 0;
 	heads = 0;
-	fairness = 0;
+	tails = 0;
     }
 
-    // setting default face for sake of testing
+    //input n is integer less than 10
+    public void setFairness(double n) {
+	fairness = n;
+    }
+
     public void setFace() {
-	face = "Head";
+	face = "heads";
+    }
+    public void setFace(String tails) {
+	face = "tails";
     }
 
-    public void addCount() {
+    public String getFlips() {
+	return "Number of flips: " + flips;
+    }
+
+    public String getFaces() {
+	return "Current face: " + face + "\n"
+	    + "Number of heads: " + heads + "\n"
+	    + "Number of tails: " + tails + "\n";
+    }
+
+    public void flip() {
+	if (Math.random() <= fairness) {
+		face = "heads";
+		heads = heads + 1;
+	    }
+	else {
+	    face = "tails";
+	    tails = tails + 1;
+	}
 	flips = flips + 1;
+	
     }
-
-    /*
-
-    public int getHeads() {
-	return heads;
-    }
-
-    public int getTails() {
-	return flips - heads;
-    }
-
-    */
-
-    public int getFlips() {
-	return flips;
-    }
-
-    public String getFace() {
-	return face;
-    }
-
 }
