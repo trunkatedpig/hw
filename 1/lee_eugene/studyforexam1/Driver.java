18d42844c938ad34b9e7c69cfe6bfaf6a12e6ec5
Andrew Fischer <afischer15@mac.com>
2013-10-05 17:57:04 -0400

    added study stuff

--- 1/lee_eugene/studyforexam1/Driver.java
+++ 1/lee_eugene/studyforexam1/Driver.java
@@ -19,6 +19,25 @@ public class Driver {
 	System.out.println(B.getName() + "'s speed is " + B.getSpeed());
 	System.out.println(B.getName() + "'s age is " + B.getAge());
 
+       	///////////////////
+	System.out.println("_____________________")
+	
+	Person p1, p2;
+	
+	p1 = new Person("Señor Garfinkel");
+	p1.setTurtle(A);
+	System.out.println(p1.getOwnerName() + "'s pet turtle is named " + p1.getPet().getName());
+	System.out.println(p1.getOwnerName() + "'s pet turtle's speed is " + p1.getTurtleSpeed());
+	System.out.println(p1.getOwnerName() + "'s pet turtle's age is " + p1.getPet().getAge());
+	
+	p2 = new Person("Edric Huang");
+	p2.setTurtle(B);
+	System.out.println(p2.getOwnerName() + "'s pet turtle is named " + p2.getPet().getName());
+	System.out.println(p2.getOwnerName() + "'s pet turtle's speed is " + p2.getTurtleSpeed());
+	System.out.println(p2.getOwnerName() + "'s pet turtle's age is " + p2.getPet().getAge());
+
+
+
 
     }
 }
