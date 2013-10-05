8b1b9d3660137ce384259fd0701fa525bde23791
sapphacs13 <sappha.omeara@gmail.com>
2013-10-05 13:46:03 -0400

    Update CoinDriver.java

--- 1/o_meara_sappha/5-coin/CoinDriver.java
+++ 1/o_meara_sappha/5-coin/CoinDriver.java
@@ -4,6 +4,47 @@ import java.util.*;
 public class CoinDriver {
     
     public static void main (String[] args) {
+	Coin c1;
+	c1 = new Coin();
+	/*
+	System.out.println(c1.flip());
+	System.out.println(c1.getFlips());
 
+	*/
+	
+	// the flip() and getFlips() methods work in Coin
+	// the constructor with no parameters works
+
+	Coin c2;
+	c2 = new Coin("quarter", 0.25);
+	/*
+	System.out.println(c2.getValue());
+	System.out.println(c2.flip());
+	System.out.println(c2.flip());
+	System.out.println(c2.flip());
+	System.out.println(c2.flip());
+	System.out.println(c2.flip());
+	System.out.println(c2.flip());
+	System.out.println(c2.flip());
+	System.out.println(c2.getFlips());
+	System.out.println(c2.getHeads());
+	System.out.println(c2.getTails());
+	*/
+	
+	// the getHeads(), getTails() methods, and the fairness in flip() work
+	// the setValue() and constructor with parameters work
+
+	CoinPurse cp1;
+	cp1 = new CoinPurse();
+	/*System.out.println(cp1.getAmount());
+	cp1.addTo(c1);
+	cp1.removeFrom(c2);
+	System.out.println(cp1.getAmount());
+	*/
+	
+	// addTo(coin), removeFrom(coin), and getAmount() methods work
+	// from CoinPurse
+	
     }
 }
+
