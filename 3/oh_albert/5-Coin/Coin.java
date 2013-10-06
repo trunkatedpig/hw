bdf2bc0229d05ef47622c28f41833b615e43b5ac
albertoh61 <albertodoh61@gmail.com>
2013-10-06 17:37:27 -0400

    Did monday's assignment and improved all three classes

--- 3/oh_albert/5-Coin/Coin.java
+++ 3/oh_albert/5-Coin/Coin.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 public class Coin {
     
-    private String faceValue = "Heads";
+    private String faceValue;
     private int heads, tails, flips;
     private double probHead = 0.5;
+    private int coinValue;
+    private String coinName;
 
     public void resetValues(String f, Double p) {
 	heads = 0;
@@ -13,21 +15,28 @@ public class Coin {
 	flips = 0;
 	faceValue = f;
 	probHead = p;
+	coinValue = 0;
     }
 
     public Coin() {
-	resetValues("Heads","0.5");
+	resetValues("Heads",0.5);
     }
 
     public Coin(String f) {
 	resetValues(f,0.5);
     }
     
-    public String getHead() {
+    public String getFace() {
 	return faceValue;
     }
 
-    public boolean equals(Coin other) {
-	return faceValue.equals(other.getFace());
+    public Coin (String coinname, int coinval) {
+	coinName = coinname;
+	coinValue = coinval;
     }
+
+    public int money () {
+	return coinValue;
+    }
+
 }
--- 3/oh_albert/5-Coin/Driver.java
+++ 3/oh_albert/5-Coin/Driver.java
@@ -4,8 +4,14 @@ import java.util.*;
 public class Driver {
     public static void main(String [] args) {
 	Coin head1 = new Coin();
-	String s = new String("Tails");
-	Coin tail = new Coin(s);
-
+	Coin tail1 = new Coin("Tails");
+        System.out.println(head1.getFace());
+	System.out.println(tail1.getFace());
+	Coin head2 = new Coin("Nickel",5);
+	Coin head3 = new Coin("Quarter",25);
+	coinPurse quarter = new coinPurse ("Quarter",25);
+	coinPurse dime = new coinPurse ("Dime",10);
+	System.out.println(quarter.addTo(head2));
+	System.out.println(dime.removeFrom(head3));
     }
 }
