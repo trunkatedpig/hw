d57e8eb33a92f4a022af25d9a3a390cef86c42a4
l-tn3314 <l.tn3314@gmail.com>
2013-10-05 22:41:10 -0400

    modified Coin.java and Driver.java

--- 2/lee_tina/7-coinPurse/Driver.java
+++ 2/lee_tina/7-coinPurse/Driver.java
@@ -5,10 +5,11 @@ import java.util.*;
 
 public class Driver {
     public static void main (String[] args) {
-	Coin c;
-	c = new Coin();
-	c.resetCount();
-	System.out.println(c.coinsEqual());
+        Coin c;
+	c = new Coin("dime");
+	//c.resetCount();
+	//System.out.println(c.coinsEqual());
 	//System.out.println(c.flipCoin());
+	System.out.println(c.val());
     }
 }
