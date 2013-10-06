83dbbe41dfe329678a31fe0436d722679c3fcb85
Vincent Wong <vwong57@gmail.com>
2013-10-05 23:53:30 -0400

    coin

--- 1/wong_vincent/5-coin/Driver.java
+++ 1/wong_vincent/5-coin/Driver.java
@@ -3,13 +3,13 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args) {
-	Coin c = new Coin();
+	Coin c = new Coin(1);
 	c.flip();
+	System.out.println ("You flipped a " + c.getName() + " which is worth " + c.getValue() + " cent(s)");
 	System.out.println (c.getFace());
 	System.out.println ("# OF HEADS:" + " " + c.getHeads());
 	System.out.println ("# OF TAILS:" + " " + c.getTails());
-	//c.reset();
-	//System.out.println(c.getFace());
+	System.out.println ("Ratio of heads to total: " + c.getFairness());
     }
 }
 
