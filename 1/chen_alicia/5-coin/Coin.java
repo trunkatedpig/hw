803266827d37bf8b6d8443914e5519c0e5a84509
Alicia Chen <mail4aliciac@gmail.com>
2013-10-05 21:31:54 -0400

    updated class work

--- 1/chen_alicia/5-coin/Coin.java
+++ 1/chen_alicia/5-coin/Coin.java
@@ -3,17 +3,39 @@ import java.util.*;
 
 public class Coin {
     private String face; //"heads" or "tails"
-    private int flips, heads, tails;
-    private int fairness; //percent of heads
+    private int tosses, heads;
+    private double prob = 0.5; //percent of heads
+
+    public void initcoin (String f, double p){
+	face = f;
+	tosses = 0;
+	heads = 0;
+	prob = p;
+    }
+
+    public Coin(){
+	initcoin("Heads",0.5);
+    }
+
+
+    public Coin(String f) {
+	initcoin(f,0.5);
+    }
 
     public void resetCounts(){
-	flips = 0;
+	tosses = 0;
 	heads = 0;
-	tails = 0;
     }
-    
+
     public String getFace() {
 	return face;
     }
 
+    public boolean equals(Coin other) {
+	return face.equals(other.getFace());
+    }
+
+    public String flip(Coin c) {
+	
+	
 }
--- 1/chen_alicia/5-coin/Driver.java
+++ 1/chen_alicia/5-coin/Driver.java
@@ -3,8 +3,36 @@ import java.util.*;
 
 public class Driver {
     public static void main (String[] args) {
-	Coin one;
-	one = new Coin();
-	System.out.println(one.getFace());
+	Coin head1 = new Coin();
+	String s = new String("Tails");
+	Coin tail1 = new Coin(s);
+	s = new String ("Heads");
+	Coin head2 = new Coin (s);
+
+
+	System.out.println(head1.getFace()+" "+tail1.getFace()+" "+head2.getFace());
+
+	System.out.println(head1.equals(tail1));
+	System.out.println(head1.equals(head2));
+	System.out.println(tail1.equals(head2));
+	System.out.println(head2.equals(head1));
+	
+	System.out.println();
+	
+	String s1 = "abcde";
+	String s2 = "defghi";
+	
+	System.out.println(s1.compareTo(s2));
+	System.out.println(s2.compareTo(s1));
+	System.out.println(s1.compareTo(s1));
+	
+	System.out.println("a".compareTo("b"));
+	System.out.println("b".compareTo("a"));
+	System.out.println("A".compareTo("a"));
+
     }
+    double r
+	r=new Random (10);
+	System.out.println (r);
+
 }
