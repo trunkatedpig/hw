6aac6667e162a000adfb899155351938e602a08e
Vincent Wong <vwong57@gmail.com>
2013-10-05 23:11:29 -0400

    coin

--- 1/wong_vincent/5-coin/Driver.java
+++ 1/wong_vincent/5-coin/Driver.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class Driver {
     public static void main(String[] args) {
 	Coin c = new Coin();
-	c.setFace ("Heads");
+	c.flip();
 	System.out.println (c.getFace());
-	c.reset();
-	System.out.println(c.getFace());
+	System.out.println ("# OF HEADS:" + " " + c.getHeads());
+	System.out.println ("# OF TAILS:" + " " + c.getTails());
+	//c.reset();
+	//System.out.println(c.getFace());
     }
 }
 
