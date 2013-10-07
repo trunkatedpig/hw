514e52c183e62aecaca30cc9520b0c0b8b2221b1
albertoh61 <albertodoh61@gmail.com>
2013-10-07 18:51:19 -0400

    Fixed StringStuff and added Driver

--- 3/oh_albert/8-StringStuff/StringStuff.java
+++ 3/oh_albert/8-StringStuff/StringStuff.java
@@ -5,16 +5,16 @@ public class StringStuff {
 
     public String capitalize (String name) {
 	int length = name.length();
-	int lnamelastletter = length - 1;
 	int space = name.indexOf(" ");
         int lastletter = space - 1;
-	String fname = name.substring(0);
-	String fnamelast = name.substring(1,lastletter);
+	String fname = name.substring(0,1);
+	String fnamelast = name.substring(1,space);
 	String fnamecap = fname.toUpperCase();
 	int firstletter = space + 1;
-	String lname = name.substring(firstletter);
+	int firstletter2 = firstletter + 1;
+	String lname = name.substring(firstletter,firstletter2);
 	int lnameafter = firstletter + 1;
-	String lnamelast = name.substring(lnameafter,lnamelastletter);
+	String lnamelast = name.substring(lnameafter,length);
 	String lnamecap = lname.toUpperCase();
 	String finalname = fnamecap + fnamelast + " " + lnamecap + lnamelast;
 	return finalname;
@@ -22,12 +22,10 @@ public class StringStuff {
 
     public String bondify (String name) {
 	int space = name.indexOf(" ");
-	int fnll = space - 1;
 	int lnfl = space + 1;
 	int length = name.length();
-	int lnll = length - 1;
-	String first = name.substring(0,fnll);
-	String last = name.substring(lnfl,lnll);
+	String first = name.substring(0,space);
+	String last = name.substring(lnfl,length);
 	String bname = last + "," + " " + first + " " + last;
 	return bname;
     }
