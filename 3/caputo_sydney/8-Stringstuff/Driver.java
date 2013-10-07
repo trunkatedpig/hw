abbe8115723d25ad4409490b772eec92d3a84159
Sydney Caputo <sydneyacaputo@gmail.com>
2013-10-07 19:19:39 -0400

    added hw 8

---
+++ 3/caputo_sydney/8-Stringstuff/Driver.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver {
+    public static void main(String[] args){
+	StringStuff s;
+	s= new StringStuff();
+	System.out.println(s.capitalize("sydney caputo"));
+	System.out.println(s.bondify("sydney caputo"));
+
+
+}
+
+
+}
