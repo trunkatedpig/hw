2f9b324290534ebb2574529ffe7f60c6cde26edd
Sam Kim <samkim319@gmail.com>
2013-10-07 01:17:11 -0400

    hw7

--- 2/kim_sam/7-coin-purse/CoinPurse.java
+++ 2/kim_sam/7-coin-purse/CoinPurse.java
@@ -3,15 +3,14 @@ import java.util.*;
 
 public class CoinPurse {
 
-    private int amount;
-    private Coin coin;
+    private double amount;
 
-    public void addTo(Coin c1) {
-	amount += c1.getValue();
+    public void addTo(Coin c) {
+	amount += c.getValue();
     }
 
-    public void removeFrom(Coin c1) {
-	amount -= c1.getValue();
+    public void removeFrom(Coin c) {
+	amount -= c.getValue();
     }
 
 
