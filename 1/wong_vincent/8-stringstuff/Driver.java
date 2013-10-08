a4262292a6f88c91794c70d1400d3e27a31f3026
Vincent Wong <vwong57@gmail.com>
2013-10-07 22:20:31 -0400

    added hw8

---
+++ 1/wong_vincent/8-stringstuff/Driver.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main (String[] args) {
+	StringStuff a = new StringStuff ("elvis presley");
+	System.out.println (a.capitalize());
+	System.out.println (a.bondify());
+    }
+}
+	
