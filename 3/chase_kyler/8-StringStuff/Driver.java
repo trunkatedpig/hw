d2e228d2a800bda9b45fb12dd35131521865c3e4
Kyler James Chase <kylerchase85@Kylers-MacBook-Air-2.local>
2013-10-07 23:31:00 -0400

    added homework #8 on strings

---
+++ 3/chase_kyler/8-StringStuff/Driver.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver {
+	public static void main(String[] args) {
+		StringStuff SS = new StringStuff();
+		String n = new String("james bond");
+		System.out.println(SS.capitalize(n));
+		System.out.println(SS.bondify(n));
+
+
+
+	}
+}
