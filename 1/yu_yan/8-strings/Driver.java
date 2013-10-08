dfcb5fb49e6ac4bbda44643bf6d8de90f90a4e3e
Vanessa <yu100997@gmail.com>
2013-10-07 23:41:56 -0400

    finished hw8

---
+++ 1/yu_yan/8-strings/Driver.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+    public static void main(String[] args){
+
+	stringStuff n;
+	n = new stringStuff();
+	System.out.println(n.capitalize("mike zamansky"));
+	System.out.println(n.bondify("mike zamansky"));
+    }
+}
