8e33580213fa922f2e5d5c930e018934c246fddf
Emma Lou <louemma2@gmail.com>
2013-10-08 19:50:51 -0400

    First part of Piglatinify done

--- 1/lou_emma/8-string-stuff/Driver.java
+++ 1/lou_emma/8-string-stuff/Driver.java
@@ -4,12 +4,18 @@ import java.util.*;
 public class Driver{
 
     public static void main (String [] args){
-		StringStuff s1;		
+		/*StringStuff s1;		
 		s1=new StringStuff();
 		String namae1="emma lou";
 		s1.indexHolder(namae1);
 		System.out.println(s1.capitalize(namae1));
 		
-		System.out.println(s1.bondify(namae1));
+		System.out.println(s1.bondify(namae1));*/
+		
+		pigLatinify pl1;
+		pl1=new pigLatinify();
+		System.out.println(pl1.pigLatin("cookies"));
+		System.out.println(pl1.pigLatin("emus"));
+		System.out.println(pl1.pigLatin("apples"));
 	}
 }
--- 1/lou_emma/8-string-stuff/pigLatinify.java
+++ 1/lou_emma/8-string-stuff/pigLatinify.java
@@ -10,12 +10,11 @@ public class pigLatinify{
 	public String pigLatin(String word){
 		word.toLowerCase();
 		String s1=word.substring(0,1);
-		if (s1=="a"||s1=="e"||s1=="i"||s1=="o"||s1=="u"){
-			word=word+"ay";
+		if (s1.equals("a")||s1.equals("e")||s1.equals("i")||s1.equals("o")||s1.equals("u")){
+			return word+"ay";
 		}
 		else {
-			word=word.substring(0)+s1+"ay";
+			return word.substring(1)+s1+"ay";
 		}
-		return word;
 	}
 }
