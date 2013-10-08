14baa0be39023314f0b3a58faba1a3dfd1da8b73
eshoyfer <eshoyfer@gmail.com>
2013-10-08 18:36:06 -0400

    Added pigLatinify in HW8

--- 3/shoyfer_elvin/8-string-stuff/StringStuff.java
+++ 3/shoyfer_elvin/8-string-stuff/StringStuff.java
@@ -21,4 +21,22 @@ public class StringStuff {
 		String lastName = name.substring(space + 1);
 		return lastName + ", " + firstName + " " + lastName;
 	}
+
+	private boolean isVowel(String l) {
+		l = l.toUpperCase(); // Vowel will always be checked as an uppercase
+		return (l.equals("A") || l.equals("E") || l.equals("I") || l.equals("O") || l.equals("U"));
+	}
+
+	public String pigLatinify(String word) {
+		// if first letter is vowel, add "yay" to the end
+		// if first letter is consonant, add (first letter + "ay") to the end
+		String firstLetter = word.substring(0,1);
+		String restOfWord = word.substring(1);
+		if (isVowel(firstLetter)) {
+			return word + "yay";
+		} else {
+			String addition = firstLetter + "ay";
+			return restOfWord + addition;
+		}
+	}
 }
