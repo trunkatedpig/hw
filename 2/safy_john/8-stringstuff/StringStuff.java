0001a8c2b17dba6da1fc5d4ecbb98a02804d8102
John Safy <jsafy1996@gmail.com>
2013-10-07 21:51:55 -0400

    hw8

---
+++ 2/safy_john/8-stringstuff/StringStuff.java
@@ -0,0 +1,15 @@
+//I'm getting a "not a statement" error when I compile this on lines 4 & 7; I'm not sure why.
+public class StringStuff{
+    public String first(String name){
+        name.substring(0,1).toUpperCase() + name.split(" ")[0].substring(1);
+    }
+    public String last(String name){
+        name.split(" ")[1].substring(0,1).toUppercase() + name.split(" ")[1].substring(1);
+    }
+    public String capitalize(String name){
+	return first + " " + last;
+    }
+    public String bondify(String name){
+	return last + ", " + name;
+    }
+}
