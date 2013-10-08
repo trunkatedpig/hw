4657071faa38b2f5e9ecf2de794fe5514dc910bb
Jamie Yip <jnyip96@gmail.com>
2013-10-08 09:26:47 -0400

    pig latin

--- 2/yip_jamie/8-StringStuff/Driver.java
+++ 2/yip_jamie/8-StringStuff/Driver.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Driver {
     public static void main (String args[]) {
 	StringStuff n= new StringStuff ();
+	PigLatinfy y= new PigLatinfy();
 	System.out.println(n.capitalize("jamie yip"));
 	System.out.println(n.bondify("jamie yip"));
+	System.out.println(y.PigLatinify("hello"));
     }
 }
