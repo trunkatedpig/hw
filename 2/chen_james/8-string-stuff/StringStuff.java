0117d751f662c493a6de70bbf42de3c52a4afc7f
James Chen <thejamesschen@gmail.com>
2013-10-08 19:51:42 -0400

    added addon to hw 8

--- 2/chen_james/8-string-stuff/StringStuff.java
+++ 2/chen_james/8-string-stuff/StringStuff.java
@@ -37,6 +37,34 @@ public class StringStuff {
 	public String bondify(String name) {
 		return getLastName(name) + ", " + getFirstName(name) + " " + getLastName(name);
 	}
+	
+	public static String capitalize(String name)
+	{
+		return name.substring(0,1).toUpperCase() + name.split(" ")[0].substring(1) + " " + name.split(" ")[1].substring(0,1).toUpperCase() + name.split(" ")[1].substring(1);
+	}
+	
+	public static String bondify(String name)
+	{
+		return name.split(" ")[1] + ", " + name;
+	}
+	
+	public static String pigLatinify(String word)
+	{
+		if(isVowel(word.charAt(0)))
+		{
+			return word + "yay";
+		}
+		else
+		{
+			return word.substring(1) + word.charAt(0) + "ay";
+		}		
+		
+	}
+
+	public static boolean isVowel(char a)
+	{
+		return a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'y';
+	}
 
 
 }
