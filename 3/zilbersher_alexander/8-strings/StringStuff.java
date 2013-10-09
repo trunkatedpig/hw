aa2b93d6e47b236441562fc47d23d630dd59ca3c
Alex Zilbersher <azilby@gmail.com>
2013-10-08 22:37:48 -0400

    added piglatinify

--- 3/zilbersher_alexander/8-strings/StringStuff.java
+++ 3/zilbersher_alexander/8-strings/StringStuff.java
@@ -19,16 +19,37 @@ String s1.indexOf(" ") will print out location of " " (ie: 5)
 public class StringStuff{
     
     public String capitalize(String name){
-	return name.substring(0,1).toUpperCase() 
-	    + name.substring(1,name.indexOf(" ")) + " " 
-	    + name.substring((name.indexOf(" ")+1),(name.indexOf(" ")+2)).toUpperCase() 
-	    + name.substring((name.indexOf(" ")+2));
+	String firstcap = name.substring(0,1).toUpperCase();
+	String wordA = firstcap + name.substring(1,name.indexOf(" "));
+	String secondcap = name.substring((name.indexOf(" ")+1),(name.indexOf(" ")+2)).toUpperCase();
+	String wordB = secondcap + name.substring((name.indexOf(" ")+2));
+	return wordA + " " + wordB;
     }
 
     public String bondify(String name){
-	String a = name.substring(0,name.indexOf(" "));
-        String b = name.substring(name.indexOf(" ")+1);
-	return b + " " + a + " " + b;
+	String wordA = name.substring(0,name.indexOf(" "));
+        String wordB = name.substring(name.indexOf(" ")+1);
+	return wordB + " " + wordA + " " + wordB;
     }
-
+    
+    public String pigLatinify(String name){
+	String firstlet = name.substring(0,1);
+	String first2let = name.substring(0,2);
+	String rest1 = name.substring(1);
+	String rest2 = name.substring(2);
+	if (firstlet.indexOf("a") == 0 ||
+	    firstlet.indexOf("e") == 0 ||
+	    firstlet.indexOf("i") == 0 ||
+	    firstlet.indexOf("o") == 0 ||
+	    firstlet.indexOf("u") == 0 ||
+	    firstlet.indexOf("y") == 0) {
+	    return name + "ay";
+        } else if (first2let.indexOf("th") == 0 ||
+		   first2let.indexOf("sh") == 0 ||
+		   first2let.indexOf("ch") == 0) {
+	    return rest2 + first2let + "ay";
+	} else {
+	    return rest1 + firstlet + "ay";
+	}
+    }
 }
