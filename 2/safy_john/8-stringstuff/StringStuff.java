08639ae1211855f49d04fae43e714359f0e5d966
John Safy <jsafy1996@gmail.com>
2013-10-08 21:03:34 -0400

    Pig latin

--- 2/safy_john/8-stringstuff/StringStuff.java
+++ 2/safy_john/8-stringstuff/StringStuff.java
@@ -1,5 +1,6 @@
 //I'm getting a "not a statement" error when I compile this on lines 4 & 7; I'm not sure why.
 public class StringStuff{
+   
     public String first(String name){
         name.substring(0,1).toUpperCase() + name.split(" ")[0].substring(1);
     }
@@ -10,6 +11,13 @@ public class StringStuff{
 	return first + " " + last;
     }
     public String bondify(String name){
-	return last + ", " + name;
+	return last + ", " + first + " "  + last;
     }
+    public String PigLatinify(String word){
+	if (word.substring == a, e, i, o, u) {
+	    return word + "yay";}
+	 else {
+	    return word.substring(1) + word.substring(0,1) + "ay";
+	    //whatdoidosomanyerrors:(
+	}}
 }
