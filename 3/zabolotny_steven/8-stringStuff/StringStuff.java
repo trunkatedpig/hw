6c29ac733d25c6bbf2cb45d77797aa8244ce9029
StevenZabolotny <steven.zabolotny@cslab1-28.stuy.edu>
2013-10-08 20:32:17 -0400

    Updated for piglatinify.

--- 3/zabolotny_steven/8-stringStuff/StringStuff.java
+++ 3/zabolotny_steven/8-stringStuff/StringStuff.java
@@ -16,4 +16,24 @@ public class StringStuff {
 	String sname = name.substring(space + 1);
 	return sname + ", " + fname + " " + sname;
     }
+    public String PigLatinify(String name) {
+	int a = name.indexOf("a");
+	int e = name.indexOf("e");
+	int i = name.indexOf("i");
+	int o = name.indexOf("o");
+	int u = name.indexOf("u");
+	int ae = Math.min(a,e);
+	int aei = Math.min(ae,i);
+	int aeio = Math.min(aei,o);
+	int firstvowel = Math.min(aeio,u);
+	if (firstvowel == 0) {
+		name = name + "yay";
+	    }
+	else {
+	    String firstgroup = name.substring(0,firstvowel);
+	    String postgroup = name.substring(firstvowel);
+	    name = postgroup + firstgroup + "ay";
+	}
+	return name;
+    }
 }
