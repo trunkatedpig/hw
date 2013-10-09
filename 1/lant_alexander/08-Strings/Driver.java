cd108590e738e7293ffbbc23ea30dc61d56e22d6
Caspar Lant <casparlant@gmail.com>
2013-10-08 21:16:45 -0400

    iglatinpay

---
+++ 1/lant_alexander/08-Strings/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main (String [] args){
+	p = new Piglatin();
+	
+	p.Piglatin("ooze");
+	p.Piglatin("trash");
+	p.Piglatin("caspar");
+	p.Piglatin("nadia");
+	p.Piglatin("buffalo");
+
+    }}
