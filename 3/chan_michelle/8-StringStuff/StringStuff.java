76f43e0c6b4d5d6a2b9e2e5574a8fd53d2a36b70
Michelle Chan <ellehcimx123@gmail.com>
2013-10-08 21:59:20 -0400

    Added piglatin files

--- 3/chan_michelle/8-StringStuff/StringStuff.java
+++ 3/chan_michelle/8-StringStuff/StringStuff.java
@@ -1,4 +1,8 @@
+import java.io.*;
+import java.util.*;
+
 public class StringStuff {
+
     public String capitalize(String name){
 	String firstInitial, lastInitial;
 	firstInitial = name.substring(0,1);
@@ -16,6 +20,17 @@ public class StringStuff {
 	String bondified = lastName + ", " + capitalize(name);
 	return bondified;
     }
+
+    public String pigLatinify(String word){
+	String firstletter = word.substring(0,1).toUpperCase();
+	String restofletters = word.substring(1).toUpperCase();
+	if (firstletter.equals("A") || firstletter.equals("E") || firstletter.equals("I") ||  firstletter.equals("O")||  firstletter.equals("U"))
+	    { return word + "yay";
+    }
+
+    else {
+	String piggy = firstletter.toLowerCase() + "ay";
+	return restofletters.toLowerCase() + piggy;
+    }
+    }
 }
-
-
