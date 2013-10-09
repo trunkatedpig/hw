9634633ae701543b6ee4b15562713270ff6fd252
Dmitriy <vakana6@gmail.com>
2013-10-09 18:23:20 -0400

    hw 9 done

---
+++ 2/kagno_dmitriy/9-factoral/Driver.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+    public static void main(String[] args){
+	
+        facotial f = new facotial();
+	System.out.println(f.nfact(3));
+	System.out.println(f.nfact(4));
+	System.out.println(f.nfact(5));
+    }
+}
