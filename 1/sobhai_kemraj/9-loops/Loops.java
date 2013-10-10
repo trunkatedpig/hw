d2a88a1193f441585222d64fb16635d0b94662b7
KbhackerVGM97 <kbsobhai@gmail.com>
2013-10-10 17:01:03 -0400

    fixed GCD

--- 1/sobhai_kemraj/9-loops/Loops.java
+++ 1/sobhai_kemraj/9-loops/Loops.java
@@ -8,4 +8,26 @@ public class Loops{
 		else
 			return n*fact(n-1);
 	}
+
+	public int GCD (int a, int b){
+		int divisor, dividend,other;
+		if (a<b){
+			divisor=a;
+			dividend=b;
+			other=a;}
+		else {
+			dividend=a;
+			divisor=b;
+			other=b;}
+		while (divisor > 0){
+			if (dividend % divisor == 0) {
+				if (other %  divisor == 0) {
+					return divisor;
+				}
+			}
+			divisor = divisor - 1;
+		}
+		return 1;
+		
+	}
 }
