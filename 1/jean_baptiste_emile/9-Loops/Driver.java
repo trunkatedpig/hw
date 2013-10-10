7c5e2f6fdfe00678106c01c7e9ef30a23b52e56d
Emile Jean-Baptiste <dr.darklord1228@gmail.com>
2013-10-10 05:59:26 -0400

    Added HW 9

---
+++ 1/jean_baptiste_emile/9-Loops/Driver.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	Loops l;
+	l = new Loops();
+	System.out.println(l.fact(5));
+	System.out.println(l.fact2(5));
+    }
+}
