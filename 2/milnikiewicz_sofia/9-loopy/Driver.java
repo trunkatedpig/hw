6c48dc1aa603dabf15714fb92d907896130ac437
Fish Milnikiewicz <fmilniki@gmail.com>
2013-10-09 23:07:09 -0400

    Updated Loopy

--- 2/milnikiewicz_sofia/9-loopy/Driver.java
+++ 2/milnikiewicz_sofia/9-loopy/Driver.java
@@ -1,1 +1,14 @@
-import
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+     public static void main (String [] args){
+	 Loops t = new Loops();
+	 System.out.println(t.fact(5));
+	 System.out.println(t.fact(4));
+	 System.out.println(t.fact(3));
+	 System.out.println(t.fact2(5));
+	 System.out.println(t.fact2(4));
+	 System.out.println(t.fact2(3));
+     }
+}
--- 2/milnikiewicz_sofia/9-loopy/Loops.java
+++ 2/milnikiewicz_sofia/9-loopy/Loops.java
@@ -1,1 +1,24 @@
-import
+import java.io.*;
+import java.util.*;
+
+public class Loops {
+    private int s;
+
+    public int fact(int n){
+	if (n == 0){
+	    return 1;
+	}
+	else {
+	    return n * fact(n-1);
+	}	
+    }
+    
+    public int fact2(int n){
+	s = 1;
+	while (n>0){
+	    s = s * n;
+	    n = n - 1;
+    }
+	return s;
+    }
+}
