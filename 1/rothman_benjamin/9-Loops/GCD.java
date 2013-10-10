469e089849c11762524853cc0b1d6a5bfe8bc268
Ben <brothman7000@gmail.com>
2013-10-10 17:27:21 -0400

    Short term

---
+++ 1/rothman_benjamin/9-Loops/GCD.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class GCD {
+
+    private int gcd;
+
+ 
+    
+    public int returnGCD (int A, int B) {
+
+	B= gcd;
+
+	while ((A % B) != 0) {
+
+	    B = B-1;
+	   B = gcd;
+	   returnGCD (A, );
+
+	}
+
+	return gcd;
+
+    }
+
+}
+
