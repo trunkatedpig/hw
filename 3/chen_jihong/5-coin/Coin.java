62ef503230b07280f582c1574300a00207e865b9
David <davidc83097@gmail.com>
2013-10-11 09:39:08 -0400

    committed

--- 3/chen_jihong/5-coin/Coin.java
+++ 3/chen_jihong/5-coin/Coin.java
@@ -6,15 +6,30 @@ public class Coin {
     private int flips;
     private int heads;
     private int tails;
-    private int fair;
-    
-    public Coin (int flips,int heads,int tails,int fair){
+    private double fair;
+
+    public void  Coin(double Fair) {
 	face = "Heads";
-	flips = flips;
-	heads = heads;
-	tails = tails;
-	fair = fair;
+	flips = 0;
+	heads = 0;
+	tails = 0;
+	fair = Fair;
     }
+    
+    public void flip() {
+        Random r = new Random();
+	if (double r < 0.5) {
+		face = "Heads";
+		flips = flips + 1;
+		heads = heads + 1;
+	}
+	     else {
+		face = "Tails";
+		flips = flips +1;
+		tails = tails +1;
+	     }
+    }
+		
 
     public String getFace (){
 	return face;
