869ff6fb83f5d7800c082e64af5d1ff0c43951d3
fawnwong <fawn.wong@live.com>
2013-10-10 20:01:17 -0400

    added isPrime method

--- 2/wong_fawn/9-loops/Driver.java
+++ 2/wong_fawn/9-loops/Driver.java
@@ -6,7 +6,11 @@ public class Driver {
     	Loops a = new Loops();
         //System.out.println(a.fact(5));
         //System.out.println(a.fact2(5));
-	System.out.println(a.GCD(12,8));
-	System.out.println(a.GCD2(12,8));
+		System.out.println(a.GCD(73303,103979));
+		System.out.println(a.GCD2(73303,103979));
+		System.out.println(a.GCD(30,15));
+		System.out.println(a.GCD2(30,15));
+		System.out.println(a.isPrime(103979));
+		System.out.println(a.isPrime(8));
     }
 }
