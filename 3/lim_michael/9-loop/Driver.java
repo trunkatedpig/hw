4f3128455f14b5e78eb6a8b901929dd9a2994f9e
michael-lim <michael.lim1121@gmail.com>
2013-10-11 00:48:55 -0400

    Updated collaboration info HW9

--- 3/lim_michael/9-loop/Driver.java
+++ 3/lim_michael/9-loop/Driver.java
@@ -1,6 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
 	Loop l = new Loop();
+	//collaboration with Jason Huang and Isaac Gluck
 	System.out.println(l.fact(7));
 	System.out.println(l.fact2(7));
 	System.out.println(l.gcd(12,8));
