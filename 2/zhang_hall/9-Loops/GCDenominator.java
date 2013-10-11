8d9c9f4ff952196fb6051e06092e3d256fbb3bde
Hall <the1zhang@gmail.com>
2013-10-10 23:00:36 -0400

    Added GCD and Driver.java

--- 2/zhang_hall/9-Loops/GCDenominator.java
+++ 2/zhang_hall/9-Loops/GCDenominator.java
@@ -5,23 +5,45 @@ public class GCDenominator{
 
     public int GCD(int a, int b){
 	int counter = b;
-	if (a >= b){
-	    if (a % b == 0){
-		return b;
-	    }
-	    else{
-		counter = counter - 1;
-		if (b % counter == 0){
+	if (a <= b){
+	    return GCD(b,a);
+	}
+	while (true){
+	    if (a % counter == 0 && b % counter == 0){
 		    return counter;
 		}
-		else{
-		    return GCD(a,counter);
-		}
-	    }
+	    counter = counter - 1;
+	}
+    }
+    
+    public int GCD2(int a, int b){
+	if (a <= b){
+	    return GCD2(b,a);
+	}
+	if (a % b == 0){
+	    return b;
 	}
 	else{
-	    return GCD(b,a);
+	    return GCD2(a, (a % b));
 	}
     }
 
+    public boolean isPrime(int n){
+	if (n == 2){
+	    return true;
+	}
+	if (n % 2 == 0){
+	    return false;
+	}
+	int counter = 3;
+	while (counter <= Math.sqrt(n)){
+	    if (n % counter == 0){
+		return false;
+	    }
+	    else{
+		counter = counter + 2;
+	    }
+	}
+	return true;
+    }
 }
