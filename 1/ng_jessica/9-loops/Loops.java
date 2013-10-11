06761218aed22222751475dca849fb95f650560e
SingleDreamer <jessica@new-host-8.home>
2013-10-11 06:48:37 -0400

    hw-9.2 GCD/Prime

--- 1/ng_jessica/9-loops/Loops.java
+++ 1/ng_jessica/9-loops/Loops.java
@@ -13,4 +13,45 @@ public class Loops {
 	return i; 
     }
 
+    public static int GCD (int a, int b) {
+	int r, i;
+	i = b
+	r = a % i;
+	while (r != 0) {
+	    while (b % i == 0) {
+		i = i - 1;
+	    }
+	    r = a % i; 
+	}
+	return i; 
+    }
+
+    public static int GCD2 ( int a, int b) {
+	int i;
+	while (a % b != 0) {
+	    i = a % b;
+	    a = b;
+	    b = i;
+	}
+	return i; 
+    }
+
+    public static int isPrime? (int n) {
+	int i = n - 1;
+	int x; 
+	boolean r; 
+	r = false
+	while (r == false) {
+	    x = n % i; 
+	    if (x != 0) {
+		i = i -1; 
+	    }
+	    else {
+		r = true; 
+	    }
+	}
+	return r; 
+    }
+
+
 }
