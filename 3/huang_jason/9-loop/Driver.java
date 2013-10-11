cf81b52a6b223697f4a88ab3dd60608d89e14e3c
Jason Huang <huang.jason999@gmail.com>
2013-10-11 04:41:50 -0400

    Updated Loops with new methods

--- 3/huang_jason/9-loop/Driver.java
+++ 3/huang_jason/9-loop/Driver.java
@@ -6,8 +6,13 @@ public class Driver{
 	System.out.println(l.fact(1));
 	System.out.println(l.fact(3));
 	System.out.println(l.fact(5));
+
 	System.out.println(l.fact2(2));
 	System.out.println(l.fact2(4));
 	System.out.println(l.fact2(6));
+
+	System.out.println(l.gcd(100000000,100000001));
+	System.out.println(l.gcd2(100000000,100000001));
+	System.out.println(l.isPrime(100000001));
     }
 }
