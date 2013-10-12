050baedb021f4b5c54935de2202c114977b67361
l-tn3314 <l.tn3314@gmail.com>
2013-10-11 23:45:20 -0400

    edited CoinGame.java and Driver.java

--- 2/lee_tina/10-coinGame/Driver.java
+++ 2/lee_tina/10-coinGame/Driver.java
@@ -3,9 +3,13 @@ import java.util.*;
 
 public class Driver {
     public static void main (String[] args){
-	CoinGame c = new CoinGame();
-	c.turn();
-	System.out.println(c.getCurrentBalance1());
-	System.out.println(c.getCurrentBalance2());
-    }
+	CoinGame c1 = new CoinGame();
+	c1.turn();
+	System.out.println(c1.getCurrentBalance1());
+	System.out.println(c1.getCurrentBalance2());
+       	CoinGame c2 = new CoinGame();
+	c2.play(10);
+	System.out.println(c2.getCurrentBalance1());
+        System.out.println(c2.getCurrentBalance2());
+	}
 }
