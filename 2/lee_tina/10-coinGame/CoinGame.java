79ed3c898893152f4df2dd6f9d740183dec6d79b
l-tn3314 <l.tn3314@gmail.com>
2013-10-11 21:06:41 -0400

    added java files

---
+++ 2/lee_tina/10-coinGame/CoinGame.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class CoinGame {
+   private BankAccount p1,p2;
+   private Coin c1,c2;
+   private double pot;
+
+    public void reset() {
+	pot = 0;
+        p1.BankAccount(10);
+        p2.BankAccount(10);
+    }
+}
