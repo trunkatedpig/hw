fa12a6420c2b222b9775cf124df1f4da4c46a79f
fawnwong <fawn.wong@live.com>
2013-10-11 22:17:10 -0400

    edited GCD2 file

--- 2/wong_fawn/9-loops/Loops.java
+++ 2/wong_fawn/9-loops/Loops.java
@@ -22,42 +22,45 @@ public class Loops {
     }
 
     public int GCD(int a, int b) {
-		int smaller, count;
-		GCD = 1;
-		if (a > b) {
-		    smaller = b;
-		}
-		else{
-		    smaller = a;
-		}
-		count = smaller;
-		while (count != GCD) {
-		    if (a % count == 0 && b % count == 0){
-				GCD = count;
-		    }
-		    else count = count - 1;
-		}
-		return GCD;
+	int smaller, count;
+	GCD = 1;
+	if (a > b) {
+	    smaller = b;
+	}
+	else{
+	    smaller = a;
+	}
+	count = smaller;
+	while (count != GCD) {
+	    if (a % count == 0 && b % count == 0){
+		GCD = count;
+	    }
+	    else count = count - 1;
+	}
+	return GCD;
     }
     public int GCD2(int a, int b) {
-		int r, larger, smaller;
-		GCD = 1;
-		if (b > a) {
-		    larger = b;
-		    smaller = a;
-		}
-		else {
-		    larger = a;
-		    smaller = b;
-		}
-		if (smaller == 0) {
-		    GCD = larger;
-		}
-		else {
-		    r = larger % smaller;
-	    	GCD2(smaller,r);
-		}	
-		return GCD;
+	int r, larger, smaller;
+	GCD = 1;
+	if (b > a) {
+	    larger = b;
+	    smaller = a;
+	}
+	else {
+	    larger = a;
+	    smaller = b;
+	}
+	while (GCD != larger){
+	    if (smaller == 0) {
+		GCD = larger;
+	    }
+	    else {
+		r = larger % smaller;
+		larger = smaller;
+		smaller = r;
+	    }
+	}
+	return GCD;
     }
     public boolean isPrime(int n){
     	boolean prime;
@@ -65,12 +68,12 @@ public class Loops {
     	prime = true;
     	currentvalue = n - 1;
     	while (prime && currentvalue != 1) {
-    		if (GCD2(n, currentvalue) != 1) {
-    			prime = false;
-    		}
-    		else {
-    			currentvalue = currentvalue - 1;
-    		}
+	    if (GCD2(n, currentvalue) != 1) {
+		prime = false;
+	    }
+	    else {
+		currentvalue = currentvalue - 1;
+	    }
     	}
     	return prime;
     }
