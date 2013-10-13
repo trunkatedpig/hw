4e846e2c503b78805acee50403c29081116646ea
IsaacGluck <isaacbgluck@gmail.com>
2013-10-13 19:02:47 -0400

    I

--- 3/gluck_isaac/10-CoinGame/Driver.java
+++ 3/gluck_isaac/10-CoinGame/Driver.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class Driver {
     public static void main(String[] args) {
-	
+	BankAccount p1 = new BankAccount("test1", 250);
+	BankAccount p2 = new BankAccount("test2", 250);
     }
 }
