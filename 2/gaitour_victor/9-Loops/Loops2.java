4ef77ca65df96bc1e55b85905e171591d36f8887
Victor Gaitour <victorgaitour@yahoo.com>
2013-10-14 19:18:29 -0400

    stuff not done yet

--- 2/gaitour_victor/9-Loops/Loops2.java
+++ 2/gaitour_victor/9-Loops/Loops2.java
@@ -2,39 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Loops2{
+    private int y;
+    private int a;
+    public int GCDi(int a, int b){
+	int counter = 1;
+	int result = 0;
+	int other = 0;
+	if (a > b ) {
+	    counter = b;
+	    other = a;}
+	else 
+	    {counter=a;
+		other=b;}
+	while (other%counter != 0){
+	    counter=counter-1;}
+	return counter;
+    }
+	    
     public int GCD(int a,int b){
 	int counter = 1;
 	int result = 0;
 	int other=0;
-	if (int a > int b){
+	if ( a > b){
 	    counter=b;
 	    other=a;}
 	else
 	    {counter = a;
 		other=b;}
-	if( a % counter==0 and b % counter ==0)
-	    return counter;
-	else 
-	    return {GCD(counter-1,other)}
+	if( a % counter==0 && b % counter ==0)
+	    {return counter;}
+	else
+	    {return GCD(counter-1,other);}
     }
-    public int GDC2(int a, int b){
+    public int GCD2(int a, int b){
 	while (b>0){
-	   y=a%b;
-	   a=b; 
-	   b=y;
+	    y=a%b;
+	    a=b;
+	    b=y;
 	}
 	return a;
     }
     public boolean isPrime(int i){
-	int a = i-1;
+	a = i-1;
 	while (a>0){
-	    if (i%a==0){
+	    if (( i % a) == 0 ){
 		return false;
 	    }
-	    else
-		{a=a-1;}
-	 	}
-	return true
+	    a=a-1;}
+	return true;
     }
 }
-	
