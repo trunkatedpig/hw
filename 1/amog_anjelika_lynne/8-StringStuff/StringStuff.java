091b7e37999842b80d5f0dc57c2adcb9ee7937f3
anjelikalynne <anjelika.amog@gmail.com>
2013-10-14 18:39:03 -0400

    changed StringStuff

--- 1/amog_anjelika_lynne/8-StringStuff/StringStuff.java
+++ 1/amog_anjelika_lynne/8-StringStuff/StringStuff.java
@@ -6,10 +6,10 @@ public class StringStuff{
 	int space;
 	String first, last, f, l;
 	space = name.indexOf(" ");
-	f = name.substring(0).toUpperCase();
-	first = f + name.substring(1,space);
-	l = name.substring(space + 1).toUpperCase();
-	last = l + name.substring(space + 2);
+	f = name.substring(0);
+	first = f.toUpperCase() + name.substring(1,space);
+	l = name.substring(space + 1);
+	last = l.toUpperCase() + name.substring(space + 2);
 	return first + last;
     }
 }
