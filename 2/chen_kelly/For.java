a83568574f87ea717c491ca0ccf1fb5c2343ef6d
Kelly Chen <kellythechen@gmail.com>
2013-10-15 19:23:50 -0400

    added fors

---
+++ 2/chen_kelly/For.java
@@ -0,0 +1,11 @@
+//Tina Lee and Kelly Chen
+import java.io.*; 
+import java.util.*;
+
+public class For{
+    private int result;
+    private int n;
+    For (result = 1; n>=1; result = result * n){
+	return result;
+	    }
+    }
