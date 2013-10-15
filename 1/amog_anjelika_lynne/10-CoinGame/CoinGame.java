fa5b9403ce99505bc9ca854fa9c1ecfceaa1efb2
anjelikalynne <anjelikalynne.amog@cslab1-3.stuy.edu>
2013-10-14 22:03:17 -0400

    added hw 10

---
+++ 1/amog_anjelika_lynne/10-CoinGame/CoinGame.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class CoinGame{
+    private BankAccount player1, player2;
+    private Coin c1, c2;
+    private double pot;
+    
+    public initPot(){
+	double amt;
+	amt = Math.random();
+	
+    }
+}
