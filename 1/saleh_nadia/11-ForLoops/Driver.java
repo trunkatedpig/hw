f80b1784b42c96c2b0d6fe811a4b107bb2560dd0
Nadia <nadiasaleh26@gmail.com>
2013-10-15 23:23:17 -0400

    For loops

---
+++ 1/saleh_nadia/11-ForLoops/Driver.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main (String [] args) {
+        For a = new For ();
+        System.out.println (a.GCD (216,144));
+        a.funTest();
+    }
+}
+
