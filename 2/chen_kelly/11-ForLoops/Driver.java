ee77795d41c87e85d1dac0d736ec9853be25df2b
kellychen <kellythechen@gmail.com>
2013-10-15 20:52:50 -0400

    Added For.java Driver.java

--- 2/chen_kelly/11-ForLoops/Driver.java
+++ 2/chen_kelly/11-ForLoops/Driver.java
@@ -2,7 +2,11 @@
 import java.io.*; 
 import java.util.*;
 public class Driver{
-    public static void main (String args[]){
+    public static void main (String[] args){
+	For f;
+	f  = new For();
 
-    	System.out.println(fact(7));
+    	System.out.println(f.fact(7));
+	System.out.println(f.Reverse("hello"));
+	System.out.println(f.table(5, 3));
     }}
