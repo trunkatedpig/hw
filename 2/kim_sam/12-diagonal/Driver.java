2b4fed948fba7c4fc614df49261b80038302308a
Sam Kim <samkim319@gmail.com>
2013-10-17 18:36:02 -0400

    in the process

--- 2/kim_sam/12-diagonal/Driver.java
+++ 2/kim_sam/12-diagonal/Driver.java
@@ -6,5 +6,6 @@ public class Driver {
     public static void main (String[] args) {
 	diagonal d = new diagonal();
 	System.out.println(d.diag(5));
+	System.out.println(d.diagWord("Supercalifragilisticexpialadocious"));
     }
 }
