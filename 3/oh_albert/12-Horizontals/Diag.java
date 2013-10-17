690eaa0ffcdcd0ba08f188ce3438534af7bfc96b
albertoh61 <albertodoh61@gmail.com>
2013-10-17 10:12:42 -0400

    added files for in-class assignment

---
+++ 3/oh_albert/12-Horizontals/Diag.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Diag {
+    
+    public String Diag(int n) {
+	String start="";
+	int counter;
+	for (int m=0;m<=n;m++){
+	    for (counter=0;counter <= m;counter++) {
+		start = start + " ";
+	    }
+	    start = start + "*" + "\n";
+	}
+	return start;
+    }
+
+    public String diagWord(String w) {
+	String start="";
+	int counter;
+	String begin;
+	for (s.length(begin)=0
+    }
+}
