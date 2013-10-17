a566024f26449128a92ec6dd43a8c44685fd89fb
StevenZabolotny <steven.zabolotny@cslab1-26.stuy.edu>
2013-10-17 10:16:28 -0400

    Progress on HW #12.

---
+++ 3/zabolotny_steven/12-moreForLoops/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args) {
+	MoreForLoops m = new MoreForLoops();
+	System.out.println(m.diag(5));
+	System.out.println(m.diag("Hello"));
+    }
+}
