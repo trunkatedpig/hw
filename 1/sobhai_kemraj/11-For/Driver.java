a9b28487a9d5c0d4aec0231761cc3c94fa64f44e
KbhackerVGM97 <kbsobhai@gmail.com>
2013-10-16 22:23:33 -0400

    Working questions one and two

---
+++ 1/sobhai_kemraj/11-For/Driver.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+	public static void main (String[] args){
+		For forloop;
+
+		forloop = new For();
+
+		String output = forloop.Reverse("Hello");
+		System.out.println(output);
+	}
+
+}
