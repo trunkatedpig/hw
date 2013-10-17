1727897bd7b75dac3cef08f7fde946139c7335c7
rukumar333 <rukumar333@gmail.com>
2013-10-16 22:41:08 -0400

    not done yet

--- 3/kumar_rushil/11-forloops/Loops.java
+++ 3/kumar_rushil/11-forloops/Loops.java
@@ -1,3 +1,13 @@
-public class Loops {
+public class Loops{
 
-    public String Reverse (String n){
+    public String reverse (String n){
+	int x;
+	String s = "";
+	for (x = n.length(); x>0 ; x --){
+	    s = s + n.substring(x-1);
+	    n = n.substring(0,x-1);
+	}
+	return s;
+    }
+
+}
--- 3/kumar_rushil/Loops.java
+++
@@ -1,7 +0,0 @@
-public class Loops{
-
-    public String reverse (String n){
-	int x;
-	String s;
-	for (x = n.length;x>1;x --){
-s = s + n.sub
