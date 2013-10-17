a097f0194162d18b34c0fbfa02d7aba835f484c9
Justin Weltz <jdnweltz@gmail.com>
2013-10-17 18:38:34 -0400

    hw12

---
+++ 1/weltz_justin/12-ForLoops2/Driver.java
@@ -0,0 +1,12 @@
+public class Driver{
+    public static void main(String[] args){
+	ForLoops x = new ForLoops();
+	System.out.println(x.diagonal(5));
+	System.out.println(x.diagonalword("justin"));
+	System.out.println(x.fence(5,8));
+	System.out.println(x.fence(2,2));
+	System.out.println(x.fence(50,50));
+	System.out.println(x.fence(1,1));
+	System.out.println(x.fence(3,3));
+    }
+}
