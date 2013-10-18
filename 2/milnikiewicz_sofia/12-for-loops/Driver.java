f58a9a5c02cdc030360dc5e221b09ee87ae6c4a3
Fish Milnikiewicz <fmilniki@gmail.com>
2013-10-17 20:55:41 -0400

    Renumbered Stuff

---
+++ 2/milnikiewicz_sofia/12-for-loops/Driver.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+    public static void main(String[] args) {
+        pants p =  new pants();
+        System.out.println(p.diag(3));
+    }
+}
