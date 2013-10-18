495ba287c780a9349be5888817249058893ca7cb
James Chen <thejamesschen@gmail.com>
2013-10-17 23:55:12 -0400

    updated hw

--- 2/chen_james/12-moreloops/Driver.java
+++ 2/chen_james/12-moreloops/Driver.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Driver{
     public static void main(String[] args){
 	Moreloops m = new Moreloops();
-	System.out.println(m.diag(10));
+	//System.out.println(m.makeEndRow(5,8));
+	//System.out.println(m.makeMidRow(5,8));
+	System.out.println(m.fence(5,8));
     }
 }
