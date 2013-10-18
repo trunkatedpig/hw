0c3332d2f495ca21aa8137636e125ef5c533dea7
Caspar Lant <casparlant@gmail.com>
2013-10-17 22:02:34 -0400

    S'more Loops

--- 1/lant_alexander/12-Moreloops/Moreloops.java
+++ 1/lant_alexander/12-Moreloops/Moreloops.java
@@ -3,13 +3,12 @@ public class Moreloops{
     public String Diagonal(int x){
 
 	String fin = "";
-	String spaces;
-	for (int n = 1; n = x; n ++){
+	String spaces = "";
+	for (int n = 1; n == x; n ++){
 	    spaces = spaces + " ";
-	    fin = fin + /n  + spaces + "*";
+	    fin = fin + "/r"  + spaces + "*";
 	}
-	
-
-
-
+	return fin;
+    }
+    
 }
