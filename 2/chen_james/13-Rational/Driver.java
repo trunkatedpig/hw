da69c1944c03bbc59e2162469279990ccac307c0
James Chen <thejamesschen@gmail.com>
2013-10-20 14:05:27 -0400

    stuck on hw 13

--- 2/chen_james/13-Rational/Driver.java
+++ 2/chen_james/13-Rational/Driver.java
@@ -5,6 +5,8 @@ public class Driver {
 
 	public static void main(String[] args) {
 		Rational x = new Rational(5,3);
-		System.out.println(x.gcd(5,3));
+		Rational y = new Rational(9,6);
+		System.out.println(x.equals(y));
+		System.out.println(x.compareTo(y));
 	}
 }
--- 2/chen_james/13-Rational/Rational.java
+++ 2/chen_james/13-Rational/Rational.java
@@ -10,9 +10,7 @@ private double result;
 public Rational(int x, int z) {
 	a = x;
 	b = z;
-	result = a / b;
-	
-	
+	result = x / z;
 }
 
 public int gcd(int a,int b) {
@@ -23,19 +21,48 @@ public int gcd(int a,int b) {
 	} 
 }
     
+//not sure how to implement this	
 public void reduce() {
-	int n = gcd(a,b);
-	a = a / n;
-	b = b / n;
+	a = a / gcd(a,b);
+	b = b / gcd(a,b);
 }
+//returns True if the Rational number represented by other is the same as the one 
+//represented by this.
 
 public boolean equals(Rational other) {
-	other.reduce();
-	t
+	
+	 if (result==other.result)
+            return true;
+        else {
+            return false;
+			}
+}
+/*create and return a new Rational that is the result of multiplying other and this. 
+For example if the two rational numbers were 4/3 and 1/2 the 
+mult routine would create a new Rational number representing 4/6 (or 2/3 when reduced). */
 
+ public Rational mult(Rational other){
+        int c = a * other.a;
+        int d = b * other.b;
+        Rational newresult = new Rational(c,d);
+        return newresult;
+}
+
+ /*returns 0 if this and other represent equal rational numbers, 
+ a positive value if this represents a rational
+ number greater than the one represented by other and 
+ a negative number is returned otherwise. */
+ public int compareTo(Rational other){
+        if (this.equals(other))
+            return 0;
+        else {
+            if (result > other.result)
+                return 1;
+            else {
+                return -1;}}}
 }
 
 
 
 
-}
+
