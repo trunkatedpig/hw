0920d1b63508dcd24506048a8d0e60ef217b9dc9
aliciachen <mail4aliciac@gmail.com>
2013-10-21 02:22:37 -0400

    completed hw 13

--- 1/chen_alicia/12-moreForLoops/Driver.java
+++ 1/chen_alicia/12-moreForLoops/Driver.java
@@ -4,11 +4,8 @@ import java.util.*;
 public class Driver{
     public static void main (String [] args){
 	For f = new For();
-	System.out.println(f.factFor(5));
-	System.out.println(f.Reverse("Hello"));
-	System.out.println(f.table(5,3));
-		   
+	System.out.println(f.diag(5));
+	System.out.println(f.diagWord("hello"));
+	System.out.println(f.fence(5,8));
     }
 }
-
-	
