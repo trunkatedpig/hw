e9e9931b51bee6dee5a56143c829edad447d20dd
anjelikalynne <anjelikalynne.amog@cslab1-3.stuy.edu>
2013-10-20 20:02:39 -0400

    finished hw 13

--- 1/amog_anjelika_lynne/13-Rational/Driver.java
+++ 1/amog_anjelika_lynne/13-Rational/Driver.java
@@ -3,11 +3,16 @@ import java.util.*;
 
 public class Driver {
     public static void main (String[] args){
-	Rational r = new Rational(3,4);
-	Rational t = new Rational(6,8);
-	int n = t.getNumerator();
-	int d = t.getDenominator();
-	t.gcd(n,d);
-	System.out.println("" + t);
+	Rational r = new Rational(4,3);
+	Rational t = new Rational(8,6);
+	System.out.println(r.equals(t));
+	Rational q = new Rational(1,2);
+	System.out.println(r.equals(q));
+	Rational s = r.mult(q);
+	System.out.println(s.printRational());
+	System.out.println(r.compareTo(t));
+	System.out.println(r.compareTo(q));
+	Rational u = new Rational(5,3);
+	System.out.println(r.compareTo(u));
     }
 }
