29d52015601b4401e2cf12046a12e5d26f7a1c20
aliciachen <mail4aliciac@gmail.com>
2013-10-21 00:48:12 -0400

    completed hw 12

--- 1/chen_alicia/11-For-Loop/Driver.java
+++ 1/chen_alicia/11-For-Loop/Driver.java
@@ -7,6 +7,9 @@ public class Driver{
 	System.out.println(f.factFor(5));
 	System.out.println(f.Reverse("Hello"));
 	System.out.println(f.table(5,3));
+	System.out.println(f.diag(5));
+	System.out.println(f.diagWord("hello"));
+	System.out.println(f.fence(5,8));		   
     }
 }
 
