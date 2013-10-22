319f4cbd0b253c1b76c26d88c5472a0f3466218d
Jason Huang <huang.jason999@gmail.com>
2013-10-21 20:33:34 -0400

    Added Rational

--- 3/huang_jason/13-Rational/Driver.java
+++ 3/huang_jason/13-Rational/Driver.java
@@ -4,7 +4,7 @@ public class Driver{
 
 	Rational abc = new Rational(20,25);
 	Rational def = new Rational(4,5);
-	Rational ghi = new Rational(1,2);
+	Rational ghi = new Rational(1,24);
 
 	System.out.println(abc.equals(def));
 	System.out.println(abc.equals(ghi));
