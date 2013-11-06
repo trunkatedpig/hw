38d99c5fdc3f4b7302b1cf05d1c8d659947e86a8
Jason Huang <huang.jason999@gmail.com>
2013-11-06 06:00:41 -0500

    Added Stringbat Array practice

---
+++ 3/huang_jason/17-Arrays1/Arrays.java
@@ -0,0 +1,17 @@
+public int[] middleWay(int[] a, int[] b) {
+  int[] c = new int[2];
+  
+  c[0] = a[1];
+  c[1] = b[1];
+  
+  return c;
+}
+
+public int[] makeEnds(int[] nums) {
+  int[] n = new int[2];
+  
+  a[0] = nums[0];
+  a[1] = nums[nums.length - 1];
+  
+  return n;
+}
