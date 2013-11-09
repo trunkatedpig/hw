07694448d9450482a85ac3661103efc2daa05a54
Caspar Lant <casparlant@gmail.com>
2013-11-08 23:51:25 -0500

    Arrays

--- 1/lant_alexander/17-Arrays/Arrays.java
+++ 1/lant_alexander/17-Arrays/Arrays.java
@@ -14,26 +14,38 @@ public class Arrays {
      * RevFill
      */      
     
-    public int[] FizzArray (int n){
-	
+    public int[] FizzArray (int n){	
 	int[] intArray = new int[n];
-
 	for(int i = 0; i < n; i ++) {
 	    intArray[i] = i;
-	    }
-	
+	}
 	return intArray;
     }
-
-    public int[] Bang(){
-    	int[] array = new int[6];
-	for(int i = 0; i < 5; i ++){
-	    array[i] = i;
-	    //getarray(array[i]);
+    
+    public boolean only14(int[] nums) {
+	for(int i = 0; i < nums.length; i ++){
+	    if (nums[i] != 1 && nums [i] != 4)
+		return false;
 	}
-	return array;
-
-   }
+	return true;
+	
+    }
+    
+    public int[] makeEnds(int[] nums) {
+	int[] ret = new int[2];
+	ret[0] = nums[0];
+	ret[1] = nums[nums.length - 1];
+	return ret;  
+    }
+    
+    public int[] middleWay(int[] a, int[] b) {
+	int ret[] = new int[2];
+	ret[0] = a[1];
+	ret[1] = b[1];
+	return ret;
+}
+    
+    
    
 
     
