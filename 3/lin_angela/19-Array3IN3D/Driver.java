693a6f924c16a16bbe53b93af04ab0d6b4f1cbd9
Angela <angela97lin@gmail.com>
2013-11-10 00:44:54 -0500

    added Array3 IN 3D

---
+++ 3/lin_angela/19-Array3IN3D/Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+    public static void main(String[] args){
+
+    Array3 a3 = new Array3();
+    System.out.println(a3);
+    System.out.println(a3.mean());
+    System.out.println(a3.stddev());
+
+
+
+
+    }
+
+}
