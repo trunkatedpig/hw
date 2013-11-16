e1a4b1d6078337ab9826b0247ce41bb1740ad57d
Fish Milnikiewicz <fmilniki@gmail.com>
2013-11-16 14:59:52 -0500

    Awwww yeahhhh

---
+++ 2/milnikiewicz_sofia/21-my-list/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+    public static void main(String[] args) {
+        myList L = new myList();
+	System.out.println("\nOriginal Array:");
+        System.out.println(L);
+	System.out.println("\nModified Array:");
+        L.insert(3,1337);
+        System.out.println(L+"\n");
+    }
+
+
+}
