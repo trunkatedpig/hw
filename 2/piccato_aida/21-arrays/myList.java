f6d2ec4e201efc60d57838401ec73dae9b4de918
Aida Piccato <aidapiccato@gmail.com>
2013-11-16 17:53:29 -0500

    Added insert method

--- 2/piccato_aida/21-arrays/myList.java
+++ 2/piccato_aida/21-arrays/myList.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class myList {
     private int[] data;
     private int numItems;
@@ -7,14 +10,36 @@ public class myList {
 	numItems = 0;
     }
 
-    public add(int d) {
-	if (numItems >= data.length) {
-	    int[] tempArray = new int[data.length + data.length/2];
+    public String toString() {
+        String s = Arrays.toString(data)+" : "+numItems;
+        return s;
+    }
+
+    public void add(int d) {
+	int[] tempArray = new int[data.length + d];
+	if (numItems <= data.length) {
+	    int total = data.length + d;
+	    System.out.println("Growing from "+data.length+" to "+total);
 	    for (int i = 0; i < data.length; i++) {
 		tempArray[i] = data[i];
-	    }
+	    }	    
 	}
-	
-	}
+	data = tempArray;
     }
+
+    public void insert(int pos, int d) {
+	int[] tempArray = new int[data.length + 1];
+	int i = 0;
+	while (i < pos) {
+	    tempArray[i] = data[i];
+	    i = i + 1;
+	}
+	tempArray[pos] = d;
+	for (int j = pos + 1; j < data.length; j++) {
+	    tempArray[j-pos] = data[j];
+	}
+	data = tempArray;
+	numItems = numItems + 1;
+    }
+    
 }
