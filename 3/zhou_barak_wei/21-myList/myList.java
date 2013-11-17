b71796c42145694646c0cd8c1bc3c17ff05bf216
Merkuri <bz2118111@gmail.com>
2013-11-16 22:51:08 -0500

    hw21

--- 3/zhou_barak_wei/21-myList/myList.java
+++ 3/zhou_barak_wei/21-myList/myList.java
@@ -1,16 +1,14 @@
 public class myList {	
-	public static int remove(int pos) {
-		int remove = a[pos];
+	public static int[] remove(int[]a, int pos) {
 		int[] retArray = new int[a.length-1];
 		for (int i=0; i<pos; i++)
 			retArray[i] = a[i];
-		for (int i=pos; i<a.length; i++)
+		for (int i=pos+1; i<a.length; i++)
 			retArray[i-1] = a[i];
-		a = retArray;
-		return remove;
+		return retArray;
 	}
 		
-	public static void insert(int[] a, int pos, int d) {
+	public static int[] insert(int[] a, int pos, int d) {
 		int[] retArray = new int[a.length+1];
 		for (int i=0; i<pos; i++){
 			retArray[i] = a[i];
@@ -19,6 +17,6 @@ public class myList {
 		for (int i=pos+1; i<a.length; i++) {
 			retArray[i+1] = a[i];
 		}
-		a = retArray;
+		return retArray;
 	}
 }
