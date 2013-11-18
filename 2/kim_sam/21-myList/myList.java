ef98550a0c85ccec5d0f7431ce608d091a2ad0aa
Sam Kim <samkim319@gmail.com>
2013-11-18 15:22:10 -0500

    asdf

--- 2/kim_sam/21-myList/myList.java
+++ 2/kim_sam/21-myList/myList.java
@@ -10,22 +10,26 @@ public class myList {
 	numItems=0;
     }
 
-    public void add(int d) {
-	
-	if (numItems >= data.length) {
-	    System.out.println("Growing from " + data.length + " to " + data.length*1.5);
+    public boolean isFull() {
+	return numItems >= data.length;
+    }
+    
+    public void grow() {
 	    // "grow" the array by creating a new one and copying over
-	    int[] tmpArray = new int[data.length + data.length/2];
-
-	    for (int i = 0; i < data.length; i++) {
-		tmpArray[i] = data[i];
-	    }
+	    int[] tmpArray = new int[data.length+data.length/2];
+	    for (int i=0;i<data.length;i++) 
+		tmpArray[i]=data[i];
 	    data = tmpArray;
+    }
+
+    public void add(int d) {
+	if (isFull()){
+	    grow();
 	}
         data[numItems]=d;
         numItems = numItems + 1;
     }
-
+    
     public void insert(int pos, int d) {
 	int[] tmpArray = new int[data.length+1];
 	for (int i = 0; i < pos; i++) {
