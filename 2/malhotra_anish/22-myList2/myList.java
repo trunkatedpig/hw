5cf38274bf90242880b2f497f8dcdcaf6a1ddcbb
Anish Malhotra <anishmalhotra97@gmail.com>
2013-11-18 18:48:14 -0500

    Made some changes to 22

--- 2/malhotra_anish/22-myList2/myList.java
+++ 2/malhotra_anish/22-myList2/myList.java
@@ -15,11 +15,11 @@ public class myList {
     }
 
     public void grow() {
-	    // "grow" the array by creating a new one and copying over
-	    int[] tmpArray = new int[data.length+data.length/2];
-	    for (int i=0;i<data.length;i++) 
-		tmpArray[i]=data[i];
-	    data = tmpArray;
+	// "grow" the array by creating a new one and copying over
+	int[] tmpArray = new int[data.length+data.length/2];
+	for (int i=0;i<data.length;i++) 
+	     tmpArray[i]=data[i];
+	data = tmpArray;
     }
 
     public void add(int d) {
@@ -36,38 +36,43 @@ public class myList {
     }
 	
     public void add(int pos, int d) {
-		// to be completed by you
-		// inserts the value d at location (index) pos)
-		// recall the algorithm we developed in calss:
-		// 1. Grow if needed
-		// 2. shift everything past the insertion point down
-		// 3. add the new item
-		// 4. numitems++
+	if (numItems < data.length){
+	    for (int i= pos+1; i<data.length; i++){
+	        data[i] = data[i-1];
+	    }
+	    data[pos] = d;
+	    numItems++;
 	}
+	else{
+	    insert(pos, d);
+	}
+    }
 
     public void remove(int pos) {
-		int[] result = new int[data.length];
-		for(int i=0;i<pos;i++){
-			result[i]=data[i];
+	int[] result = new int[data.length];
+	for(int i=0;i<pos;i++){
+		result[i]=data[i];
+	}
+	for(int i=pos;i<result.length;i++){
+		if(i==(result.length-1)){
+			result[i]=0;
+			break;
 		}
-		for(int i=pos;i<result.length;i++){
-			if(i==(result.length-1)){
-				result[i]=0;
-				break;
-			}
-			result[i]=data[i+1];
-		}
-		data = result;
-		numItems--;
-		
+		result[i]=data[i+1];
 	}
-	public int size() {
-		return numItems;
-	}
-	public int get(int pos) {
-		return data[pos];
-	}
-	public void set(int pos, int d) {
-		data[pos] = d;
-	}
+	data = result;
+	numItems--;	
+    }
+
+    public int size() {
+	return numItems;
+    }
+
+    public int get(int pos) {
+	return data[pos];
+    }
+
+    public void set(int pos, int d) {
+	data[pos] = d;
+    }
 }
