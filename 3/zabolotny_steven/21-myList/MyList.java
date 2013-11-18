fc951d6a79a8bd5ee4af254023156d585a6fd116
StevenZabolotny <steven.zabolotny@cslab1-28.stuy.edu>
2013-11-18 00:55:19 -0500

    HW #21

---
+++ 3/zabolotny_steven/21-myList/MyList.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class MyList {
+    private int[] data;
+    private int items = 0;
+
+    public MyList(int n) {
+	data = new int[n];
+    }
+
+    public String toString() {
+	return Arrays.toString(data);
+    }
+
+    public void add(int n) {
+	if (items == data.length) {
+	    int[] temp = new int[(int)(data.length * 1.5)];
+	    for (int i = 0;i < data.length;i++) {
+		temp[i] = data[i];
+	    }
+	    data = temp;
+	}
+	data[items] = n;
+	items = items + 1;
+    }
+
+    public void remove(int pos) {
+	for (int i = pos;i < data.length - 1;i++) {
+	    data[i] = data[i + 1];
+	}
+	items = items - 1;
+    }
+
+    public void insert(int pos,int n) {
+	add(0);
+	for (int i = items;i > pos + 1;i--) {
+	    data[i] = data[i - 1];
+	}
+	data[pos] = n;
+	items = items + 1;
+    }
+}
