da965125d32ca88ecca5713e4ed5362aff333813
Aaron <aaron@ubuntu>
2013-11-18 18:28:26 -0500

    updated myList

--- 3/mortenson_aaron/21-myList/List.java
+++ 3/mortenson_aaron/21-myList/List.java
@@ -1,18 +1,51 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class List {
-    private int[] a;
+       private int[] data;
+       private int numItems;
 
-    public List() {
-	a=new int[5];
+       public List() {
+          data = new int[5];
+	  numItems = 0;
+       }
+
+
+    public boolean isFull() {
+	return numItems >= data.length;
     }
+
+    public void grow() {
+	int[] tmpArray = new int[data.length+data.length/2];
+	for (int i=0;i<data.length;i++) {
+	    tmpArray[i]=data[i];
+	}
+	data = tmpArray;
+    }
+
+    public void add(int d) {
+	if (isFull()) {
+	    grow();
+    }
+
+	data[numItems] = d;
+	numItems=numItems+1;
+    }
+
+
+    public String toString() {
+	String s = Arrays.toString(data)+" : "+numItems;
+	return s;
+    }
+
 
     public int remove(int pos) {
-	int result = a[pos];
-	for (;pos<a.length-1;pos++)
-	    a[pos]=a[pos+1];
-	a[a.length]=0;
+	int result = data[pos];
+	for (;pos<numItems-1;pos++)
+	    data[pos]=data[pos+1];
+	numItems--;
 	return result;
     }
+}
+
 }
