3f51448bbb1c85ec24813354b59fe7b79bd57e7a
ericwongg <wonggeric@gmail.com>
2013-11-19 17:47:24 -0500

    Added myStringList

--- 2/wong_eric/21-myList/Driver.java
+++ 2/wong_eric/21-myList/Driver.java
@@ -1,6 +1,7 @@
 
 public class Driver {
     public static void main(String[] args) {
+	/*
 	myList L = new myList();
 
 	System.out.println(L);
@@ -17,13 +18,38 @@ public class Driver {
 	L.insert(2, 14);
 	System.out.println(L);
 
-	L.remove(5);
+	L.remove(6);
 	System.out.println(L);
 
 	L.find(20);
 
 	L.fremove(15);
 	System.out.println(L);
+	*/
+
+	/*
+	myStringList a = new myStringList();
+
+	a.set(0, "Hey");
+	a.set(1, "there");
+	a.set(2, "good");
+	a.set(3, "looking");
+	a.set(4, "XD");
+	
+	System.out.println(a);
+
+	a.add("SIKE");
+	System.out.println(a);
+
+	a.insert(6, "LOL");
+	System.out.println(a);
+	
+	a.remove(5);
+	System.out.println(a);
+
+	a.fremove("XD");
+	System.out.println(a);
+	*/
 
     }	
 
