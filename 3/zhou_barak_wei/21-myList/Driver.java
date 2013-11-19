6a939232fbec09e9bdc8979d79bca68b5a6de2b4
Merkuri <bz2118111@gmail.com>
2013-11-18 20:56:37 -0500

    hw22

--- 3/zhou_barak_wei/21-myList/Driver.java
+++ 3/zhou_barak_wei/21-myList/Driver.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class Driver {
 	public static void main(String[] args) {
-		int[] a = {1, 2, 3, 4, 5};
+		int[] a = {0, 1, 2, 3, 4};
 		
-		a = myList.remove(a, 4);
+		a = myList.remove(a, 0);
 		System.out.println(Arrays.toString(a));
 		
 		a = myList.insert(a, 1, 5);
