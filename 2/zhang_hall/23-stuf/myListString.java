539c9958911355c0249eb90c1d5b13667826abfc
Hall Zhang <the1zhang@gmail.com>
2013-11-19 18:50:39 -0500

    Working on converting to strings

--- 2/zhang_hall/23-stuf/myListString.java
+++ 2/zhang_hall/23-stuf/myListString.java
@@ -1,9 +1,12 @@
-public class myList {
-    private int[] data;
+import java.io.*;
+import java.util.*;
+
+public class myListString {
+    private String[] data;
     private int numItems;
     
-    public myList() {
-	data = new int[5];
+    public myListString() {
+	data = new String[5];
 	numItems=0;
     }
 
@@ -13,13 +16,13 @@ public class myList {
 
     public void grow() {
 	// "grow" the array by creating a new one and copying over
-	int[] tmpArray = new int[data.length+data.length/2];
+	String[] tmpArray = new String[data.length+data.length/2];
 	for (int i=0;i<data.length;i++) 
 	    tmpArray[i]=data[i];
 	data = tmpArray;
     }
 
-    public void add(int d) {
+    public void add(String d) {
 	if (isFull()){
 	    grow();
 	}
@@ -32,17 +35,17 @@ public class myList {
 	return s;
     }
     
-    public void add(int pos, int d) {
+    public void add(int pos, String d) {
 	if (numItems<data.length){
 	    for(int i = numItems-1; i > pos-1; i--) {
-                        data[i+1] = data[i];
-                }
-                data[pos] = d;
-                numItems = numItems + 1;    
+		data[i+1] = data[i];
+	    }
+	    data[pos] = d;
+	    numItems = numItems + 1;    
 	}
 	else{
-	    int[] a = data;
-	    add(1);
+	    String[] a = data;
+	    grow();
 	    for (int i=pos;i<data.length;i++){
 		if (i == pos){
 		    data[i] = d;}
@@ -56,15 +59,14 @@ public class myList {
 	for (int i=pos; i<numItems-1; i++) {
             data[i] = data[i + 1];
         }
-        data[numItems - 1] = 0;
         numItems--;    }
     public int size() {
 	return numItems;
     }
-    public int get(int pos) {
+    public String get(int pos) {
 	return data[pos];
     }
-    public int set(int pos, int d) {
+    public int set(int pos, String d) {
 	data[pos]=d;
 }
 }
