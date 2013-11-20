4cc71c921ad9e5cdc39c2c026ec294f53e2de239
Jeffrey He <jh1676@gmail.com>
2013-11-19 19:01:08 -0500

    22b

--- 2/he_jeffrey/22 - myList Plus/myStringList.java
+++ 2/he_jeffrey/22 - myList Plus/myStringList.java
@@ -1,13 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-public class myList {
-
-	private int[] data;
+public class myStringList {
+	private String[] data;
 	private int numItems;
 	
-	public myList() {
-		data = new int[5];
+	public myStringList() {
+		data = new String[5];
 		numItems=0;
 	}
 	public boolean isFull() {
@@ -17,13 +16,14 @@ public class myList {
 	public void grow() {
 		System.out.println("Growing from "+data.length+" to "+data.length*1.5);
 		// "grow" the array by creating a new one and copying over
-		int[] tmpArray = new int[data.length + data.length/2];
-		for (int i=0; i<data.length; i++)
-		tmpArray[i] = data[i];
+		String[] tmpArray = new String[data.length + data.length/2];
+		for (int i=0; i<data.length; i++) {
+			tmpArray[i] = data[i];
+		}
 		data = tmpArray;
 	}
 
-	public void add(int d) {
+	public void add(String d) {
 		if (isFull()) {
 			grow();
 		}
@@ -31,9 +31,9 @@ public class myList {
 		numItems++;
 	}
 	
-	public void add(int pos, int d) {
+	public void add(int pos, String d) {
 		if (isFull()) {
-			int[] ret = new int[data.length + 1];
+			String[] ret = new String[data.length + 1];
 			for (int i = 0; i < numItems + 1; i++) {
 				if (i < pos) {
 					ret[i] = data[i];
@@ -44,32 +44,34 @@ public class myList {
 					ret[pos] = d;
 				}
 			}
-			numItems++;
 			data = ret;
 		} else {
-			for (int i = numItems; i > pos; i++) {
+			for (int i = numItems; i > pos; i--) {
 				data[i] = data[i-1];
 			}
 			data[pos] = d;
 		
 		}
+		numItems++;
+
 	}
 	
 	public void remove(int pos) {
 		for (int i=pos; i<numItems-1; i++) {
 			data[i] = data[i + 1];
 		}
-		data[numItems - 1] = 0;
+		data[numItems - 1] = "";
 		numItems--;
 	}
 
-	public int get(int pos) {
+	public String get(int pos) {
 		return data[pos];
 	}
 
-	public void set(int pos, int d) {
+	public void set(int pos, String d) {
 		data[pos] = d;
 	}
+
 	public int size() {
 		return numItems;
 	}
@@ -78,22 +80,21 @@ public class myList {
 		String s = Arrays.toString(data)+" : "+numItems;
 		return s;
 	}
-    
-    public int find(int n) {
-	for (int i = 0; i < numItems; i++) {
-	    if (data[i] == n) {
-		return data[i];
-	    }
+	
+	public String find(String n) {
+		for (int i = 0; i < numItems; i++) {
+			if(data[i].equals(n)) {
+				return data[i];
+			}
+		}
+		return "";
 	}
-	return -1;
-    }
-    
-    public void fremove(int n){
-	for (int i=0;i<data.length;i++){
-	    if (data[i]==n){
-		remove(i);
-	    }
+	
+	public void fremove(String n) {
+		for (int i = 0; i < numItems; i++) {
+			if (data[i].equals(n)) {
+				remove(i);
+			}
+		}
 	}
-    }
-
 }
