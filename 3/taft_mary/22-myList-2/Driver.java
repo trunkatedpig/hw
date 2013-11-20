96ce4112d017e0f91082009d1e742f3977b676b3
MaryRosariaTaft <Mary.Taft97@gmail.com>
2013-11-19 19:51:33 -0500

    Tweaked the Driver.

--- 3/taft_mary/22-myList-2/Driver.java
+++ 3/taft_mary/22-myList-2/Driver.java
@@ -8,11 +8,11 @@ public class Driver {
 	myList L = new myList();
 
 	System.out.println(L);
-	for (int i = 0; i<5;i++)
+	for (int i = 0; i < 5; i++)
 	    L.add((i+1)*5);
 	System.out.println(L);
 
-	for (int i = 0; i<7;i++)
+	for (int i = 0; i < 7; i++)
 	    L.add((i+1)*5);
 	System.out.println(L);
 	System.out.println();
