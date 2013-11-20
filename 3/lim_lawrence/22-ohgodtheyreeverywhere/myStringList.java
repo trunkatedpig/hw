0dfbc0c9bb512dae2dc7774e92590961ad0d626b
OokamiNya <42chrono@gmail.com>
2013-11-19 19:29:02 -0500

    hw22: upgraded to lv2 stringiness

--- 3/lim_lawrence/22-ohgodtheyreeverywhere/myStringList.java
+++ 3/lim_lawrence/22-ohgodtheyreeverywhere/myStringList.java
@@ -1,29 +1,29 @@
 import java.io.*;
 import java.util.*;
 
-public class myList {
-    private int[] data;
+public class myStringList {
+    private String[] data;
     private int numItems;
     
-    public myList() {
-	data = new int[5];
+    public myStringList() {
+	data = new String[5];
 	numItems=0;
     }
 
     public int size() {return numItems;}
-    public int get(int pos) {return data[pos];}
-    public void set(int pos, int d) {data[pos]=d;}
+    public String get(int pos) {return data[pos];}
+    public void set(int pos, String d) {data[pos]=d;}
 
     public void grow() {
 	//System.out.println("Growing from " + data.length + 
 	//                   " to " + data.length*3/2);
-	int[] tmpArray = new int[data.length+data.length/2];
+	String[] tmpArray = new String[data.length+data.length/2];
 	for (int i=0;i<data.length;i++) 
 	    tmpArray[i]=data[i];
 	data = tmpArray;
     }
 
-    public void add(int d) {
+    public void add(String d) {
 	if (numItems>=data.length) {grow();}
         data[numItems]=d;
         numItems = numItems + 1;
@@ -34,36 +34,36 @@ public class myList {
 	return s;
     }
 
-    public void insert(int pos, int d) {
+    public void insert(int pos, String d) {
 	add(data[numItems-1]);
 	for(int i=numItems-1; i>pos; i--)
 	    data[i]=data[i-1];
 	data[pos]=d;
     }
 	
-    public int remove(int pos) {
-	int r=data[pos];
+    public String remove(int pos) {
+	String r=data[pos];
 	for(int i=pos;i<numItems;i++)
 	    data[i]=data[i+1];
 	numItems--;
 	return r;
     }
     
-    public int find(int n) {
+    public String find(String n) {
 	for(int i=0;i<numItems;i++) {
 	    if(data[i]==n) {return data[i];}
 	}
-	return 0;
+	return "nope";
     }
 
-    public int fremove(int n) {
+    public String fremove(String n) {
 	for(int i=0;i<numItems;i++) {
 	    if(data[i]==n) {
-		int r = data[i];
+		String r = data[i];
 		remove(i);
 		return r;
 	    }
 	}
-	return 0;
+	return "nope";
     }
 }
