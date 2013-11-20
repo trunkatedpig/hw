49a2bff57074e267f381ba14a6d612627958e56c
Vincent Wong <vwong57@gmail.com>
2013-11-20 00:24:19 -0500

    hw 21

---
+++ 1/wong_vincent/21-mylistpart1/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static void main(String[] args) {
+	MyList a = new MyList();
+
+	System.out.println(a.insert(5, 1));
+	//System.out.println(a);
+    }	    
+}
+	    
