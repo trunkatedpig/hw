275b22d6aa4da136b84146783d903b7688827997
Caspar Lant <casparlant@gmail.com>
2013-11-20 20:30:54 -0500

    MyList2

--- 1/lant_alexander/22-MyList2/Driver.java
+++ 1/lant_alexander/22-MyList2/Driver.java
@@ -7,6 +7,13 @@ public class Driver{
 	MyList2 ml2 = new MyList2();
 
 	System.out.println("caspa");
-
+	System.out.println(ml2.toString());
+	ml2.insert(10, 10);
+	System.out.println(ml2.toString());
+	System.out.println(ml2.find(10));
+	ml2.remove(12);
+	System.out.println(ml2.toString());
+	ml2.fremove(10);
+	System.out.println(ml2.toString());
     }
 }
