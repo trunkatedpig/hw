e4b71b2e9f2443ad47f14e8104be9a8123f4d37b
Bouowmx <sean.yip@cslab1-1.stuy.edu>
2013-11-24 16:10:39 -0500

    Added ArrayLists

--- 1/yip_sean/23-ArrayLists/ArrayListBenchmark.java
+++ 1/yip_sean/23-ArrayLists/ArrayListBenchmark.java
@@ -69,15 +69,15 @@ public class ArrayListBenchmark {
 		}
 		
 		System.out.println("Time to add to the end of an ArrayList of size n:");
-		for (int i = 0; i < times[0].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[0][i] + " ns/" + ((double) times[0][i] / 1000000) + " ms/" + ((double) times[0][i] / 1000000000) + " s");}
+		for (int i = 0; i < times[0].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[0][i] + " ns/" + ((double) times[0][i] / 1000000) + " ms/" + ((double) times[0][i] / 1000000000) + " s");} //Adding to end is constant time (~320 ns)
 		
 		System.out.println("\nTime to add to the beginning:");
-		for (int i = 0; i < times[1].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[1][i] + " ns/" + ((double) times[1][i] / 1000000) + " ms/" + ((double) times[1][i] / 1000000000) + " s");}
+		for (int i = 0; i < times[1].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[1][i] + " ns/" + ((double) times[1][i] / 1000000) + " ms/" + ((double) times[1][i] / 1000000000) + " s");} //Adding to beginning exponentially increases in time as the number of elements increases
 		
 		System.out.println("\nTime to remove from the end: ");
-		for (int i = 0; i < times[2].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[2][i] + " ns/" + ((double) times[2][i] / 1000000) + " ms/" + ((double) times[2][i] / 1000000000) + " s");}
+		for (int i = 0; i < times[2].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[2][i] + " ns/" + ((double) times[2][i] / 1000000) + " ms/" + ((double) times[2][i] / 1000000000) + " s");} //Removing from end is constant time (~320 ns)
 		
 		System.out.println("\nTime to remove from the beginning:");
-		for (int i = 0; i < times[3].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[3][i] + " ns/" + ((double) times[3][i] / 1000000) + " ms/" + ((double) times[3][i] / 1000000000) + " s");}
+		for (int i = 0; i < times[3].length; i++) {System.out.println((long) Math.pow(10, i + 1) + ": " + times[3][i] + " ns/" + ((double) times[3][i] / 1000000) + " ms/" + ((double) times[3][i] / 1000000000) + " s");} //Removing from beginning exponentially increases in time as the number of elements increases
 	}
 }
