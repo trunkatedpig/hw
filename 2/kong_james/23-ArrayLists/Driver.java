95cca861edc97e03d6f52c2c45cf8970ac3485b7
jameskong <jdetect1@gmail.com>
2013-11-24 18:20:31 -0500

    hw 23

--- 2/kong_james/23-ArrayLists/Driver.java
+++ 2/kong_james/23-ArrayLists/Driver.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class Driver{
     public static void main(String[] args){
-	ArrayList<Integer> front = new ArrayList<Integer>();
+	/* Part 1
+	   ArrayList<Integer> front = new ArrayList<Integer>();
 	ArrayList<Integer> back = new ArrayList<Integer>();
 	long start = System.currentTimeMillis();
 
-	/*Part 1*/
-	/*Adding to back is faster than adding to the front*/
+	Adding to back is faster than adding to the front
 	for (int i=0; i  < 100000;i++){
 	    front.add(0,i);
 	}
@@ -19,8 +19,8 @@ public class Driver{
 	}
 	System.out.println("Adding to back: "+(System.currentTimeMillis()-start)) ;
 
-	/*Part 2*/
-	/*Removing from the back was also faster than removing from the front*/
+	Part 2
+	Removing from the back was also faster than removing from the front
 	start = System.currentTimeMillis();
 	for (int i = 0; i < front.size();i++){
 	    front.remove(0);
@@ -33,7 +33,7 @@ public class Driver{
 	}
 	System.out.println("Removing from the back: "+ (System.currentTimeMillis()- start));
     
-	/*Part 3*/
+	Part 3*/
 	ArrayList<Integer> newarray = new ArrayList<Integer>();
 	for (int i = 0; i < 10; i ++){
 	    newarray.add(i);
