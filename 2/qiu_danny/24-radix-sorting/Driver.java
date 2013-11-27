f1a724aa0a3e7a2b12710d2a047d7e0b8bfa6ca6
Danny Qiu <dqiu55@gmail.com>
2013-11-27 09:02:01 -0500

    Add timing to compare radix and built-in quicksort

--- 2/qiu_danny/24-radix-sorting/Driver.java
+++ 2/qiu_danny/24-radix-sorting/Driver.java
@@ -5,18 +5,30 @@ import java.util.*;
 public class Driver {
 
     public static void main(String[] args) {
+        int arraysLength, maxValue;
+        if (args.length == 2) {
+            arraysLength = Integer.parseInt(args[0]);
+            maxValue = Integer.parseInt(args[1]) + 1;
+        }
+        else {
+            arraysLength = 1000000;
+            maxValue = Integer.MAX_VALUE;
+        }
+
         Random r = new Random();
+        long start, end;
 
         ArrayList[] buckets = new ArrayList[10];
         for (int i=0; i<buckets.length; i++) {
             buckets[i] = new ArrayList();
         }
 
-
-        int[] numbers = new int[10000000];
+        int[] numbers = new int[arraysLength];
         for (int i=0; i<numbers.length; i++) {
-            numbers[i] = r.nextInt(Integer.MAX_VALUE);
+            numbers[i] = r.nextInt(maxValue);
         }
+        int[] numbersCopy = numbers;
+        //System.out.println(Arrays.toString(numbers));
 
         int power = 0;
         for (int i=0; i<numbers.length; i++) {
@@ -29,7 +41,7 @@ public class Driver {
             maxPower++;
         }
 
-        System.out.println(Arrays.toString(numbers));
+        start = System.currentTimeMillis();
         for (int n=0; n<maxPower; n++) {
             for (int i=0; i<numbers.length; i++) {
                 int digit = (int) (numbers[i] / Math.pow(10, n)) % 10;
@@ -46,7 +58,14 @@ public class Driver {
                 buckets[i] = new ArrayList();
             }
         }
+        end = System.currentTimeMillis();
+        //System.out.println(Arrays.toString(numbers));
+        System.out.println(end - start);
 
-        System.out.println(Arrays.toString(numbers));
+        start = System.currentTimeMillis();
+        Arrays.sort(numbersCopy);
+        end = System.currentTimeMillis();
+        //System.out.println(Arrays.toString(numbersCopy));
+        System.out.println(end - start);
     }
 }
