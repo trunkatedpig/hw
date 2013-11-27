08af9322712d50fb8169327b7298cd197236e165
Andrew Sikowitz <nysiko@gmail.com>
2013-11-26 22:58:19 -0500

    Stuff

---
+++ 1/sikowitz_andrew/24-Stuff/Radix.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Radix {
+    public static void main(String[] args) {
+	ArrayList[] buckets = new ArrayList[10];
+
+	int a[] = new int[100];
+	Random r = new Random();
+	int digit;
+	int counter;
+
+	for (int i=0; i<100; i++)
+	    a[i] = r.nextInt(0, 10000);
+
+	for (int n=0; n<4; n++) {
+	    for (int i=0; i<10; i++)
+		buckets[i] = new ArrayList();
+	    for (int i=0; i<100; i++) {
+		digit = (int)(a[i] / Math.pow(10, n)) % 10;
+		buckets[digit].add(a[i]);
+	    }
+	    counter = 0;
+	    for (int i=0; i<10; i++) {
+		while (buckets[i].size() > 0) {
+		    a[counter] = buckets[i].get(buckets[i].size()-1);
+		    buckets[i].remove(buckets[i].size()-1);
+		}
+	    }
+	}
+	System.out.println(a);
+    }
+}
