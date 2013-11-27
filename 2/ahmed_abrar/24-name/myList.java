8a14f30f331e0ac3cdfdb2a9bd19be4c288fc311
Abrar Ahmed <abrarahmed264@yahoo.com>
2013-11-27 09:26:13 -0500

    24

--- 2/ahmed_abrar/24-name/myList.java
+++ 2/ahmed_abrar/24-name/myList.java
@@ -4,22 +4,30 @@ import java.util.*;
 public class myList {
     public static void main ( String[] args ) {
 	Random r = new Random();
-	for ( int i = 0 ; i < 10 ; i++ )
-	    buckets [ i ] = new ArrayList();
-	int[] randstuff=new int[20];
+	ArrayList[] buckets = new ArrayList [ 10 ];
+	int[] randstuff=new int[200000];
 	for (int i = 0;i<randstuff.length;i++)
-	    randstuff[i]=r.nextInt(9999-1000)+1000;
-	int digits = 4;
+	    randstuff[i]=r.nextInt(899999999)+100000000;
+	int digits = 9;
+	long start = System.currentTimeMillis();
 	for(int i = 0;i<digits;i++){
-	    ArrayList[] buckets = new ArrayList [ 10 ];
+	    for ( int l = 0 ; l < 10 ; l++ )
+		buckets [ l ] = new ArrayList();
 	    for(int j =0;j<randstuff.length;j++){
 		int k = (int) (randstuff[j]/(Math.pow(10,i))) % 10;
 		buckets[k].add(randstuff[j]);
 	    }
 	    System.out.println(Arrays.toString(buckets));
-	    System.out.println("THIS IS THE END");
+	    System.out.println("THIS IS THE NEXT DIGIT");
 	    
 	}
-	System.out.println(Math.pow(10,3));
+	long time = System.currentTimeMillis() - start;
+	System.out.println ( time );
+
+	start = System.currentTimeMillis();
+	Arrays.sort ( randstuff );
+	time = System.currentTimeMillis() - start;
+	System.out.println ( time );
+	
     }
 }
