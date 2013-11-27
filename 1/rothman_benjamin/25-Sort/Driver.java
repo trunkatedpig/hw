32658e84a393ad0418ec0074494dbc283d1838a4
Ben <brothman7000@gmail.com>
2013-11-26 20:20:59 -0500

    HW 25

--- 1/rothman_benjamin/25-Sort/Driver.java
+++ 1/rothman_benjamin/25-Sort/Driver.java
@@ -5,67 +5,31 @@ public class Driver {
 
     public static void main (String [] args) {
 
-	Random random = new Random ();
-	int numItems = 0;
 
-	int  [] numbers = new int [10];
-	for (int i = 0; i < 10; i ++) {
-
-	    numbers [i] = random.nextInt (10) + random.nextInt (10) * 10 + random.nextInt (10) * 100 + random.nextInt (10) * 1000;
-
-	    numItems = numItems + 1;
-
-	}
+	Sorting s = new Sorting (10,4); //First number says how many elements there will be in the array. The second is how many digits in the number.
 
 
-	System.out.println (Arrays.toString (numbers) );
-
-	ArrayList [] buckets = new ArrayList [10];
-
-	for (int i = 0; i < numbers.length; i ++) {
-	    buckets [i] = new ArrayList ();
-	}
-
-	int digits = String.valueOf (numbers [0] ).length ();
-
-
-	for (int n =0; n < digits; n ++) {
-	    for (int i = 0; i < numbers.length; i ++) {
-
-
-	    int teller = 0;
-	    
-	    teller =( numbers [i] / (math.pow (10, n) ) ) % 10;
-
-	    buckets [teller].add (numbers [i] );
-
-	    numItems = numItems - 1;
-
-	    }
-
-	    for (int i = 0; i < numbers.length; i ++ ) {
-
-		int cholata = 0;
-		cholata = buckets [i].size ();
-
-		while (cholata > 0) {
-
-		    numbers [numItems-1] = buckets [i].get (0);
-		    buckets [i].remove (0);
-
-		}
-
-
-	    }
-
-	 
-
-	}
+	System.out.println (s);
 
 
 
+	s.Order ();
 
-	System.out.println (Arrays.toString (numbers));
+
+	System.out.println (s);
+
+
+	System.out.println ("------------");
+
+
+	Sorting s2 = new Sorting (10, 6);
+
+	System.out.println (s2);
+
+
+	s2.Order ();
+
+	System.out.println (s2);
 
     }
 
