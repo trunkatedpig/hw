825e803fe2f98bd67d7505675e0fb6806db3954a
unknown <twang.123@live.com>
2013-11-29 15:40:20 -0500

    Added timer

--- 1/wang_tianshi/24-Buckets/Buckets.java
+++ 1/wang_tianshi/24-Buckets/Buckets.java
@@ -17,22 +17,22 @@ public class Buckets {
         int n = 0;
         while (n < 3) {
             for (int i = 0; i < Test.length; i++) {
-                System.out.print(Test[i] + " (mod " + Math.pow(10,n)+")");
+                //System.out.print(Test[i] + " (mod " + Math.pow(10,n)+")");
                 int placeholder = (int) ((Test[i]/Math.pow(10,n)) % 10);
-		System.out.println(" --> " + placeholder);
+		//System.out.println(" --> " + placeholder);
                 buckets[placeholder].add(Test[i]); 
             }
             int[] tempArray = new int[Test.length];
             int x = 0;
             for (int j = 0; j< buckets.length; j++){
-                System.out.println(j+ ":"+buckets[j].size());
+                //System.out.println(j+ ":"+buckets[j].size());
                 for(int k = 0; k < buckets[j].size(); k++) {
                     tempArray[x] = (Integer)buckets[j].get(k);
                     x = x + 1;
                     }
             }
             Test = tempArray;
-            System.out.println(Arrays.toString(Test));
+            //System.out.println(Arrays.toString(Test));
             for (int i = 0; i < 10; i++) 
                 buckets[i] = new ArrayList();
             n = n + 1;
--- 1/wang_tianshi/24-Buckets/Driver.java
+++ 1/wang_tianshi/24-Buckets/Driver.java
@@ -4,15 +4,25 @@ import java.util.*;
 public class Driver{
     
     public static void main(String[] args) {
+
         Buckets j = new Buckets();
         System.out.println(j);
+	long start = System.currentTimeMillis();
         j.sort();
+	long end = System.currentTimeMillis();
+        System.out.println(j);
+	System.out.println("Time elapsed: "+(end - start) + " milliseconds");
+
+
 
         System.out.println();
 
 	int[] a = {5,3,67,2,11,787,3,6,1};
 	System.out.println(Arrays.toString(a));
+	start = System.currentTimeMillis();
 	Arrays.sort(a);
+	end = System.currentTimeMillis();
 	System.out.println(Arrays.toString(a));
+	System.out.println("Time elapsed: "+(end - start) + " milliseconds");
     }
 }
