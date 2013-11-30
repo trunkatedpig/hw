0bb1805e6cc64a0b5ecc93adf25a8d8807f572b3
sapphacs13 <sappha.omeara@gmail.com>
2013-11-30 16:13:11 -0500

    Update Radix.java

--- 1/o_meara_sappha/24-Radix/Radix.java
+++ 1/o_meara_sappha/24-Radix/Radix.java
@@ -5,15 +5,15 @@ public class Radix{
     public int[] sort(int[] A){
         ArrayList[] buckets = new ArrayList[10];
         int n = 0;
-        while (n<4){
-            for(int i =0;i<10;i++)
+        while (n < 4){
+            for(int i = 0; i < 10; i ++)
                 buckets[i] = new ArrayList();
-            for (int j=0;j<A.length;j++){
+            for (int j = 0; j < A.length; j ++){
                 buckets[(A[j] / (int)Math.pow(10,n)) % 10].add(A[j]);
             }
             int counter = 0;
-            for(int p = 0; p<buckets.length; p++){
-                for(int m = 0; m<buckets[p].size(); m = m){
+            for(int p = 0; p < buckets.length; p ++){
+                for(int m = 0; m < buckets[p].size(); m = m){
                     A[counter] = (Integer)(buckets[p].remove(0));
                     counter = counter + 1;
                 }
