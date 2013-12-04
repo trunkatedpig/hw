b1f80e68abcc5991b9c88766322787c461aa805d
Merkuri <bz2118111@gmail.com>
2013-12-03 21:59:23 -0500

    hw25

---
+++ 3/zhou_barak_wei/25-words/WordSearch.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class WordSearch {
+
+    private char[][] board;
+
+    public WordSearch(int rows, int cols) {
+	board = new char[rows][cols];
+	for (int i=0;i<rows;i++) 
+	    for (int j=0;j<cols;j++) 
+		board[i][j]='-';
+    }
+
+    public WordSearch() {
+	this(20,20);
+    }
+
+    public String toString() {
+	String s = "";
+	for (int i=0;i<board.length;i++) {
+	    for (int j=0;j<board[i].length;j++) {
+		s=s+board[i][j];
+	    }
+	    s=s+"\n";
+	}
+	return s;
+    }
+	
+	public boolean addWordH(int row, int col, String word) {
+		if (row >= board.length) return false;
+		if ((board[row].length - col) < word.length()) return false; //check if the word can fit
+		int location = col;
+		int letter = 0;
+		
+		
+		while (letter < word.length()) {
+			if (board[row][location] != word.charAt(letter) ||
+				board[row][location] != '-') System.out.println("Check");
+			location = location + 1;
+			letter = letter + 1;
+		}
+		
+		
+		//at this point checking for invalidation should be complete
+		location = col;
+		letter = 0;
+		while (letter < word.length()) {
+			board[row][location] = word.charAt(letter);
+			location = location + 1;
+			letter = letter + 1;
+		} 
+		return true;
+	}
+
+
+}
