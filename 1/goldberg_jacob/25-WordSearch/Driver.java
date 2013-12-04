01bcca1f3e4b62ef3c25b75838bea6cd55961746
CobyGoldbergs <CobyGoldbergs@users.noreply.github.com>
2013-12-03 20:37:40 -0500

    Trying to hash out the falses

---
+++ 1/goldberg_jacob/25-WordSearch/Driver.java
@@ -0,0 +1,15 @@
+public class Driver{
+
+    public static void main(String[] args){
+	WordSearch x = new WordSearch(20,20);
+	System.out.println(x);
+	System.out.println(x.InsertWordH(4, 6, "Hello"));
+	System.out.println(x);
+	System.out.println(x.InsertWordH(4, 5, "Hello"));
+
+	System.out.println(x);
+	
+    } 
+
+
+}
