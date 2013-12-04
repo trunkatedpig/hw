89e42b582e24d3ded351c9963f6092c4ef420855
Victor Gaitour <victorgaitour@yahoo.com>
2013-12-03 21:43:02 -0500

    from school

---
+++ 2/gaitour_victor/WordSearch/WordSearch.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class WordSearch{
+    char[][] board= new  char[][];
+    public boolean AddwordH(int r, int c, String word){
+	int w=word.length
+	if  (c>=board.length || c<0)
+	    return false;
+	if (r>=board[0].length || r<0)
+	    return false;
+	if (w+r>board[0].length)
+	    return false;
+	
+    }
+
+}
