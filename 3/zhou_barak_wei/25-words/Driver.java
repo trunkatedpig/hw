528708eef8df5371998229c384fca232aa674fcd
Merkuri <bz2118111@gmail.com>
2013-12-04 16:23:02 -0500

    addWordV

--- 3/zhou_barak_wei/25-words/Driver.java
+++ 3/zhou_barak_wei/25-words/Driver.java
@@ -5,6 +5,7 @@ public class Driver {
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
 	w.addWordH(2, 5, "test");
+	w.addWordV(10, 10, "tester");
 	System.out.println(w);
     }
 
