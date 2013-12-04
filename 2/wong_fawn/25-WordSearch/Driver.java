a0c60cf0c2dd15527b0e593ccbfe100d4dcf054e
fawnwong <fawn.wong@live.com>
2013-12-03 20:54:50 -0500

    added WordSearch

---
+++ 2/wong_fawn/25-WordSearch/Driver.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Driver {
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	System.out.println(AddWord(0,0,"hello"));
+	System.out.println(w);
+    }
+
+}
