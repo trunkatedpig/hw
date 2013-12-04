a953f578583540231bfb008ab1ddc49c56435525
David <davidc83097@gmail.com>
2013-12-04 04:27:12 -0500

    word search addh

---
+++ 3/chen_jihong/25-wordsearch/Driver.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addwordH(10,10,"hello");
+	System.out.println(w);
+    }
+}
