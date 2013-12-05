cb4e3593d5d32af7835890b59b1862d51c44311c
Caspar Lant <casparlant@gmail.com>
2013-12-04 22:45:19 -0500

    Wordsearch

--- 1/lant_alexander/25-2DArrays/Driver.java
+++ 1/lant_alexander/25-2DArrays/Driver.java
@@ -3,8 +3,14 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args) {
-	WordSearch w = new WordSearch();
+	WordSearch w = new WordSearch(10,20);
 	System.out.println(w);
+    w.AddWordH(1, 1, "caspar");
+    w.AddWordH(1, 3, "parma");
+    w.AddWordV(3,3, "blam");
+    System.out.println(w);
+
+
     }
 
 }
--- 1/lant_alexander/25-2DArrays/WordSearch.java
+++ 1/lant_alexander/25-2DArrays/WordSearch.java
@@ -2,37 +2,72 @@ import java.util.*;
 import java.io.*;
 
 public class WordSearch {
-
+    
     private char[][] board;
     private int rows;
     private int cols;
-
+    
     public WordSearch(int r, int c) {
-    rows = r;
-    cols = c;    
-    board = new char[r][c];
+	rows = r;
+	cols = c;    
+	board = new char[r][c];
 	for (int i=0;i<r;i++) 
 	    for (int j=0;j<c;j++) 
 		board[i][j]='-';
     }
-
+    
     public WordSearch() {
 	this(20,20);
     }
-
-    public boolean InsertWord(int r, int c, String word){
-	// return #t if word is on board && is compatible with competing words
-        int length = word.length();
-        if(length > rows - r)
-        return false;
-	for(int l = 0; l < length; l++){
-	   if (board[r][c+l] != word.charAt(l));
-	    }
+    
+    public boolean AddWordH(int r, int c, String word){
+        int i = 0;
+        try{
+	    while (i < word.length()){
+		if (board[r][c+i] != '-' && board[r][c+i] != word.charAt(i))
+		    return false;
+		i ++;
+            }
+        }
+        catch (Exception e){
+            return false;
+        }
+        i = 0; 
+        while (i < word.length()){
+            board[r][c+i] = word.charAt(i);
+            i ++;
+        }
+        toString();
         return true;
+    }
+    
+    public boolean AddWordV(int r, int c, String word){
+        int i = 0;
+        try {
+            while (i < word.length()){
+                if (board[r+i][c] != '-' && board[r+i][c] != word.charAt(i))
+		    return false;
+		i ++;
+	    }
+	}
+        catch (Exception e1){
+            return false;
+        }
+	
+        i = 0;
+        while (i < word.length()){
+            board [r+i][c] = word.charAt(i);
+            i ++;
+        }
+	toString();
+        return true;
+	
+	
     }
     
     public String toString() {
 	String s = "";
+	System.out.println("");
 	for (int i=0;i<board.length;i++) {
 	    for (int j=0;j<board[i].length;j++) {
 		s=s+board[i][j];
@@ -41,6 +76,6 @@ public class WordSearch {
 	}
 	return s;
     }
-
-
+    
+    
 }
