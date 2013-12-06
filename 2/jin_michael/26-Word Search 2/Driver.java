e80be2cf24189a3c55663fcc2dcc0abdc6fdc30b
herowatch <michael.jin123@gmail.com>
2013-12-05 19:50:27 -0500

    Adding Hw26

--- 2/jin_michael/26-Word Search 2/Driver.java
+++ 2/jin_michael/26-Word Search 2/Driver.java
@@ -10,9 +10,17 @@ public class Driver {
 	System.out.println(w);
 	System.out.println(w.addWordDBR(1,2,"word"));
 	System.out.println(w);
-	System.out.println(w.addWordDBL(1,4,"word"));
+	System.out.println(w.addWordDBL(1,3,"word"));
 	System.out.println(w);
-	System.out.println(w.addWordDTL(3,16,"word"));
+	System.out.println(w.addWordDTL(14,3,"word"));
+	System.out.println(w);
+	System.out.println(w.addWordDTR(14,1,"word"));
+	System.out.println(w);
+	System.out.println(w.addWordHB(5,6,"word"));
+	System.out.println(w);
+	System.out.println(w.addWordVB(16,7,"word"));
+	System.out.println(w);
+	w.randomL();
 	System.out.println(w);
     }
 }
--- 2/jin_michael/26-Word Search 2/WordSearch.java
+++ 2/jin_michael/26-Word Search 2/WordSearch.java
@@ -52,7 +52,55 @@ public class WordSearch {
 	return true;
     }
 
-    public boolean addWordV(int row, int col, String word){
+    public boolean addWordHB(int row, int col, String word){
+	int rowmax=board.length;
+	int colmax=board[0].length;
+	int wmax=word.length();
+
+	if (row<0 || col>colmax || row>rowmax)
+	    return false;
+	if (-1>col-wmax)
+	    return false;
+
+	for (int ww=0;ww<wmax;ww++){
+	    int colf=col-ww;
+	    char w=board[row][colf];
+	    if (w!=('-') && w!=(word.charAt(ww)))
+		return false;
+	}
+
+	for (int ww=0;ww<wmax;ww++){
+	    int colf=col-ww;
+	    board[row][colf]=word.charAt(ww);
+	}
+	return true;
+    }
+
+    public boolean addWordVB(int row, int col, String word){
+	int rowmax=board.length;
+	int colmax=board[0].length;
+	int wmax=word.length();
+
+	if (row<0 || col<0 || col>colmax)
+	    return false;
+	if (-1>row-wmax)
+	    return false;
+
+	for (int ww=0;ww<wmax;ww++){
+	    int rowf=row-ww;
+	    char w=board[rowf][col];
+	    if (w!=('-') && w!=(word.charAt(ww)))
+		return false;
+	}
+
+	for (int ww=0;ww<wmax;ww++){
+	    int rowf=row-ww;
+	    board[rowf][col]=word.charAt(ww);
+	}
+	return true;
+    }
+
+ public boolean addWordV(int row, int col, String word){
 	int rowmax=board.length;
 	int colmax=board[0].length;
 	int wmax=word.length();
@@ -110,7 +158,7 @@ public boolean addWordDBR(int row, int col, String word){
 	
 	if (row<0 || col<0)
 	    return false;
-	if (rowmax<wmax+row || 0>col-wmax)
+	if (rowmax<wmax+row || -1>col-wmax)
 	    return false;
 	
 	for (int ww=0;ww<wmax;ww++){
@@ -137,7 +185,7 @@ public boolean addWordDBR(int row, int col, String word){
 	
 	if (row<0 || col<0)
 	    return false;
-	if (0>row-wmax || 0>col-wmax)
+	if (-1>row-wmax || -1>col-wmax)
 	    return false;
 	
 	for (int ww=0;ww<wmax;ww++){
@@ -155,5 +203,49 @@ public boolean addWordDBR(int row, int col, String word){
 	    board[rowf][colf]=word.charAt(ww);
 	}
 	return true;
+    }
+
+public boolean addWordDTR(int row, int col, String word){
+	int rowmax=board.length;
+	int colmax=board[0].length;
+	int wmax=word.length();
+	
+	if (row<0 || col<0)
+	    return false;
+	if (-1>row-wmax || colmax<col+wmax)
+	    return false;
+	
+	for (int ww=0;ww<wmax;ww++){
+	    int rowf=row-ww;
+	    int colf=col+ww;
+	    
+	    char w=board[rowf][colf];
+	    if (w!=('-') && w!=(word.charAt(ww)))
+		return false;
+	}
+	
+	for (int ww=0;ww<wmax;ww++){
+	    int rowf=row-ww;
+	    int colf=col+ww;
+	    board[rowf][colf]=word.charAt(ww);
+	}
+	return true;
+    }
+
+    public void randomL(){
+	int rowmax=board.length;
+	int colmax=board[0].length;
+
+	String al="abcdefghijklmnopqrstuvwxyz";
+	Random r=new Random();
+	
+	for (int i=0;i<rowmax;i++){
+	    for (int j=0;j<colmax;j++){
+		if (board[i][j]=='-'){
+			int rr=r.nextInt(26);
+			char rrr=al.charAt(rr);
+			board[i][j]=rrr;}
+	    }
+	}
     }
 }
