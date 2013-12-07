0ffc6de8cb64edeb0219433d82d78170374dd70b
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2013-12-07 15:31:07 -0500

    added methods

--- 1/sapozhnikov_brian/26-WordSearch2/Driver.java
+++ 1/sapozhnikov_brian/26-WordSearch2/Driver.java
@@ -4,9 +4,21 @@ import java.util.*;
 public class Driver {
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	System.out.println(w.addWordH(5,6,"e"));
-       	System.out.println(w);
 	System.out.println(w.addWordH(5,5,"hello"));
+	System.out.println(w);
+	System.out.println(w.addWordHLeft(5,5,"hello"));
+	System.out.println(w);
+	System.out.println(w.addWordV(5,5,"hello"));
+	System.out.println(w);
+	System.out.println(w.addWordVUp(5,5,"hello"));
+	System.out.println(w);
+	System.out.println(w.addWordSE(5,5,"hello"));
+	System.out.println(w);
+	System.out.println(w.addWordNW(5,5,"hello"));
+	System.out.println(w);
+	System.out.println(w.addWordSW(5,5,"hello"));
+	System.out.println(w);
+	System.out.println(w.addWordNE(5,5,"hello"));
 	System.out.println(w);
     }
 
