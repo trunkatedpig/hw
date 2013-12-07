6cac213f4e63a0a802508d3859b3ee4f554d0167
Sarah <sprutchenko@gmail.com>
2013-12-07 14:32:50 -0500

    thanks for deleting all of these files

---
+++ 3/prutchenko_sarah/5-coinflip/Coin.java
@@ -0,0 +1,14 @@
+public class Coin{
+    private String face;
+    private int flips;
+    private int fairness;
+
+    public int reset(){
+	flips = 0;
+    }
+    public String faceValue(){
+	return face;
+    }
+
+
+}
