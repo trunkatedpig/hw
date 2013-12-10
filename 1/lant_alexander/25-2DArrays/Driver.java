02369a50bc71b42cc5f4444006e38663ea9e37ae
Caspar Lant <casparlant@gmail.com>
2013-12-10 09:48:11 -0500

    Random instruments in random places

--- 1/lant_alexander/25-2DArrays/Driver.java
+++ 1/lant_alexander/25-2DArrays/Driver.java
@@ -7,15 +7,13 @@ import java.io.*;
 
 public class Driver {
     public static void main(String[] args) {
+	
 	WordSearch w = new WordSearch(20,40);
 	Random R = new Random();
 	//  AddWord (int r, int c, int dx, int dy, String word)
 	// dx: 1 for right, -1 for left, 0 for stationary
 	// dy: 1 for down, -1 for up, 0 for stationary
-	
-	//w.AddWord(10, 10, -1, 0, "caspar");
-	//w.AddWord(11, 10, 1, 0, "CASPAR");
-	w.RandWords(50);
+       	w.RandWords(50);
 	System.out.println(w);
 	w.FillIn();
        	System.out.println(w);	
