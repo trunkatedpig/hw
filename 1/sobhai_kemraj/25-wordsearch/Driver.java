5fe545f281b2e7f19ec712ee6f9df801ff4975cf
Kemraj Sobhai <kbsobhai@gmail.com>
2013-12-11 08:17:44 -0500

    stuff

--- 1/sobhai_kemraj/25-wordsearch/Driver.java
+++ 1/sobhai_kemraj/25-wordsearch/Driver.java
@@ -1,16 +1,21 @@
 import java.util.*;
 import java.io.*;
 
-public class Driver{
-    public static void main(String[] args){
-        WordSearch w = new WordSearch();
+public class Driver {
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch(15,30);
 	System.out.println(w);
-        System.out.println(w.addWordVup(10,10,"David"));
-        System.out.println(w);
-        System.out.println(w.addWordVup(12,14,"Bang"));
-        System.out.println(w);
-
+	w.addWordRandomLoc("defenistration");
+	w.addWordRandomLoc("hello");
+	w.addWordRandomLoc("world");
+	w.addWordRandomLoc("goat");
+	w.addWordRandomLoc("turtle");
+	w.addWordRandomLoc("koala");
+	w.addWordRandomLoc("squid");
+	w.addWordRandomLoc("doggie");
+	//System.out.println(w);
+	//w.fillInBlanks();
+	//System.out.println(w);
     }
 
 }
-
