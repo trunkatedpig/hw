f51bf9b02776d728086343dfe55e077a6d6cc962
Aaron <aaron@ubuntu>
2013-12-14 14:04:35 -0500

    changed wordsearch a bit

--- 3/mortenson_aaron/25-WordSearch/Driver.java
+++ 3/mortenson_aaron/25-WordSearch/Driver.java
@@ -1,12 +1,10 @@
 import java.util.*;
+import java.io.*;
 
 public class Driver {
-    public static void main(String[] args) {
-	WordSearch w = new WordSearch();
-	w.addWordH(12,7,"moo");
-	//	w.addWordH(10,7,"Kabloosh");
-	w.addWordH(13,7,"oopmh");
-	//	w.addWordH(10,21,"fds");
+    public static void main(String[] args){
+        
+        WordSearch w = new WordSearch(15,40);
 	System.out.println(w);
     }
 }
