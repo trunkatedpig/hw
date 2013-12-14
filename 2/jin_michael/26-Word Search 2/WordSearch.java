51123a24dfb097ac5cf0ed3eb6a7bc7c57d1baa7
Sam Kim <samkim319@gmail.com>
2013-12-14 17:44:02 -0500

    asdflkj

--- 2/jin_michael/26-Word Search 2/WordSearch.java
+++ 2/jin_michael/26-Word Search 2/WordSearch.java
@@ -1,16 +1,26 @@
 import java.util.*;
+import java.io.*;
 
 public class WordSearch {
     private char[][] board;
-
+    private Random rand;
+    private ArrayList<String> wordList;
+    
     public WordSearch(int rows, int cols) {
+	rand = new Random();
+	loadWords("wordlist.txt");
+	System.out.println(wordList+"\n");
 	board = new char[rows][cols];
 	for (int i=0;i<rows;i++) {
 	    for (int j=0;j<cols;j++) {
 		board[i][j]='-';
 	    }
 	}
-
+	for (int i=0;i<wordList.size();i++)
+	    addWordRand(wordList.get(i));
+	System.out.println(toString());
+	fillBlanks();
+	System.out.println(toString());
     }
 
     public WordSearch() {
@@ -28,6 +38,60 @@ public class WordSearch {
 	return s;
     }
 
+private void loadWords(String filename) {
+	wordList = new ArrayList<String>();
+	try {
+	    File f = new File(filename);
+	    Scanner sc = new Scanner(f);
+	    while (sc.hasNext()) {
+		String s =sc.nextLine();
+		wordList.add(s);
+	    }
+	} catch (FileNotFoundException e) {
+	    System.out.println(e);
+	    System.exit(0);
+	}
+}
+    
+    public boolean addWord(int row, int col, int deltaR,int deltaC,String word) {
+	int r,c;
+	
+	if (deltaR<-1||deltaR>1||deltaC<-1||deltaC>1||
+	    (deltaR==0&&deltaC==0))
+	    return false;
+	
+	// see if we can add the word
+	r = row;
+	c = col;
+	for (int i=0;i<word.length();i++) {
+	    try {
+		if (board[r][c]!='-' && board[r][c]!=word.charAt(i)) {
+		    return false;
+		}
+	    } catch (ArrayIndexOutOfBoundsException e) {
+		return false; // return false since we can't add the word - we're out of bounds
+	    }
+	    r=r+deltaR;
+	    c=c+deltaC;
+	}
+	r=row;
+	c=col;
+	for (int i=0;i<word.length();i++) {
+	    board[r][c]=word.charAt(i);
+	    r=r+deltaR;
+	    c=c+deltaC;
+	}
+    return true;
+}
+    
+    public boolean addWordRand(String w) {
+	int r = rand.nextInt(board.length);
+	int c = rand.nextInt(board[0].length);
+	int deltaR = rand.nextInt(3)-1;
+	int deltaC = rand.nextInt(3)-1;
+	return addWord(r,c,deltaR,deltaC,w);
+    }
+    
     public boolean addWordH(int row, int col, String word){
 	int rowmax=board.length;
 	int colmax=board[0].length;
@@ -232,7 +296,7 @@ public boolean addWordDTR(int row, int col, String word){
 	return true;
     }
 
-    public void randomL(){
+    public void fillBlanks(){
 	int rowmax=board.length;
 	int colmax=board[0].length;
 
--- 3/ruta_anna/25-wordSearch/WordSearch.java
+++ 3/ruta_anna/25-wordSearch/WordSearch.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 public class WordSearch {
     private char[][] board;
-    int maxRow, maxCol;
+    private int maxRow, maxCol;
+    private Random rand;
 
-    Random generator = new Random();
 
     public WordSearch(int rows, int cols) {
+		rand = new Random();
 		maxRow = rows;
 		maxCol = cols;
     	board = new char[rows][cols];
@@ -22,143 +23,92 @@ public class WordSearch {
         this(20,20);
     }
 
+/* Main addWord method!!
+Parameters row and col specify the location of the first character in the word.
+Parameters deltaRow and deltaCol (ranging from -1,0,1) are used to specify which
+direction the word will be written in. If it is possible to add the word to the
+board, then the word is added and method returns true*/
+	public boolean addWord(int row, int col, int deltaRow, int deltaCol, String word){
+		if (deltaRow<-1 || deltaRow>1 || deltaCol<-1 || deltaCol>1)
+			return false;
+		if (deltaRow==0 && deltaCol==0)
+			return false;
+		int r = row;
+		int c = col;
+		for (int i=0; i<word.length(); i++){
+			if (r<0 || r>=maxRow || c<0 || c>=maxCol)
+				return false;
+			if (board[r][c]!= word.charAt(i) && board[r][c] != '-')
+				return false;
+			r = r+deltaRow;
+			c = c+deltaCol;
+		}
+		r = row;
+		c = col;
+		for (int i=0; i<word.length(); i++){
+			board[r][c] = word.charAt(i);
+			r = r+deltaRow;
+			c = c+deltaCol;
+		}
+		return true;
+	}
 
-/*Will test if its possible to horizontally, left-to-right, add the word
-in the desired row and col, where row and col specify the location of the
-first character in the word. If possible, the word will be added to gameboard*/
+//Adds word horizontally right-to-left
 	public boolean addWordH(int row, int col, String word){
-		if (row<0 || col<0|| row>maxRow || col+word.length()>maxCol)
-			return false;
-		for (int i=0; i<word.length(); i++){
-			if (word.charAt(i) != board[row][col+i] && board[row][col+i] != '-')
-				return false;
-		}
-		for (int j=0; j<word.length(); j++){
-			board[row][col+j] = word.charAt(j);
-		}
-		return true;
+		return addWord(row,col,0,1,word);
 	}
 
-/*Will test if its possible to vertically, top-to-bottom, add the word in the
-desired row and col, where row and col specify the location of the first
-character in the word. If possible, the word will be added to gameboard*/
+//Adds word vertically top-to-bottom
 	public boolean addWordV(int row, int col, String word){
-		if (row<0 || col<0|| col>maxCol || row+word.length()>maxRow)
-			return false;
-		for (int i=0; i<word.length(); i++){
-			if (word.charAt(i) != board[row+i][col] && board[row+i][col] != '-')
-				return false;
-		}
-		for (int j=0; j<word.length(); j++){
-			board[row+j][col] = word.charAt(j);
-		}
-		return true;
+		return addWord(row,col,1,0,word);
 	}
 
-/*Will test if its possible to horizontally, right-to-elft, add the word
-in the desired row and col, where row and col specify the location of the
-last character in the word. If possible, the word will be added to gameboard*/
+//Adds word horizontally left-to-right
 	public boolean addWordHRev(int row, int col, String word){
-		if (row<0 || col<0|| row>maxRow || col+word.length()>maxCol)
-			return false;
-		for (int i=0; i<word.length(); i++){
-			if (word.charAt(i) != board[row][col+word.length()-1-i] && board[row][col+word.length()-1-i] != '-')
-				return false;
-		}
-		for (int j=0; j<word.length(); j++){
-			board[row][col+word.length()-1-j] = word.charAt(j);
-		}
-		return true;
+		return addWord(row,col,0,-1,word);
 	}
 
-/*Will test if its possible to vertically, bottom-to-top, add the word in the
-desired row and col, where row and col specify the location of the last
-character in the word. If possible, the word will be added to gameboard*/
-	public boolean addWordVRev(int row, int col, String word){
-		if (row<0 || col<0|| col>maxCol || row+word.length()>maxRow)
-			return false;
-		for (int i=0; i<word.length(); i++){
-			if (word.charAt(i) != board[row+word.length()-1-i][col] && board[row+word.length()-1-i][col] != '-')
-				return false;
-		}
-		for (int j=0; j<word.length(); j++){
-			board[row+word.length()-1-j][col] = word.charAt(j);
-		}
-		return true;
+//Adds word vertically bottom-to-top
+	public boolean addWordVRev(int row, int col, String word) {
+		return addWord(row,col,-1,0,word);
 	}
 
-/*Will test if its possible to diagonally, topRight-to-bottomLeft, add the word in the
-desired row and col, where row and col specify the location of the first
-character in the word. If possible, the word will be added to gameboard*/
-	public boolean addWordTRBL(int row, int col, String word){
-		if (row<0 || col<0|| row+word.length()>maxRow || col+word.length()>maxCol)
-			return false;
-		for (int i=0; i<word.length(); i++){
-			if (word.charAt(i) != board[row+i][col+i] && board[row+i][col+i] != '-')
-				return false;
-		}
-		for (int j=0; j<word.length(); j++){
-			board[row+j][col+j] = word.charAt(j);
-		}
-		return true;
+//Adds word diagonally from topRight-to-bottomLeft
+	public boolean addWordTRBL(int row, int col, String word) {
+		return addWord(row,col,1,1,word);
 	}
 
-/*Will test if its possible to diagonally, bottomLeft-to-topRight, add the word in the
-desired row and col, where row and col specify the location of the last
-character in the word. If possible, the word will be added to gameboard*/
-	public boolean addWordBLTR(int row, int col, String word){
-		if (row<0 || col<0|| row+word.length()>maxRow || col+word.length()>maxCol)
-			return false;
-		for (int i=0; i<word.length(); i++){
-			if (word.charAt(i) != board[row+word.length()-1-i][col+word.length()-1-i] && board[row+word.length()-1-i][col+word.length()-1-i] != '-')
-				return false;
-		}
-		for (int j=0; j<word.length(); j++){
-			board[row+word.length()-1-j][col+word.length()-1-j] = word.charAt(j);
-		}
-		return true;
+//Adds word diagonally from topLeft-to-bottomRight
+	public boolean addWordTLBR(int row, int col, String word) {
+		return addWord(row,col,1,-1,word);
 	}
 
-/*Will test if its possible to diagonally, topLeft-to-bottomRight, add the word in the
-desired row and col, where row and col specify the location of the first
-character in the word. If possible, the word will be added to gameboard*/
-	public boolean addWordTLBR(int row, int col, String word){
-			if (row<0 || col<0|| row+word.length()>maxRow || col>maxCol)
-				return false;
-			for (int i=0; i<word.length(); i++){
-				if (word.charAt(i) != board[row+i][col-i] && board[row+i][col-i] != '-')
-					return false;
-			}
-			for (int j=0; j<word.length(); j++){
-				board[row+j][col-j] = word.charAt(j);
-			}
-			return true;
+//Adds word diagonally from bottomRight-to-topLeft
+	public boolean addWordBRTL(int row, int col, String word) {
+		return addWord(row,col,-1,1,word);
 	}
 
-/*Will test if its possible to diagonally, bottomRight-to-TopLeft, add the word in the
-desired row and col, where row and col specify the location of the last
-character in the word. If possible, the word will be added to gameboard*/
-	public boolean addWordBRTL(int row, int col, String word){
-		if (row<0 || col<0|| row+word.length()>maxRow || col>maxCol)
-			return false;
-		for (int i=0; i<word.length(); i++){
-			if (word.charAt(i) != board[row+word.length()-1-i][col-word.length()+1+i] && board[row+word.length()-1-i][col-word.length()+1+i] != '-')
-				return false;
-		}
-		for (int j=0; j<word.length(); j++){
-			board[row+word.length()-1-j][col-word.length()+1+j] = word.charAt(j);
-		}
-		return true;
+//Adds word diagonally from bottomLeft-to-topRight
+	public boolean addWordBLTR(int row, int col, String word) {
+		return addWord(row,col,-1,-1,word);
 	}
 
+//Adds word at a random location in a random direction
+	public boolean addWordRand(String word) {
+		int row = rand.nextInt(maxRow);
+		int col = rand.nextInt(maxCol);
+		int deltaRow = rand.nextInt(2)-1;
+		int deltaCol = rand.nextInt(2)-1;
+		return addWord(row,col,deltaRow,deltaCol,word);
+	}
+
+//Fills in the empty spaces on the board with random uppercase letters
 	public void fillInBoard(){
-		int randChar;
+		rand = new Random();
 		for(int i=0; i<maxRow; i++){
 			for (int j=0; j<maxCol; j++){
-				if (board[i][j] == '-'){
-					randChar = generator.nextInt(26)+65;
-					board[i][j] = (char)randChar;
-				}
+				if (board[i][j] == '-')
+					board[i][j] = (char)(rand.nextInt(26)+65);
 			}
 		}
 	}
