ac28fd70271c64ed52538e004aa26855472bcdda
tdiep26 <timd33@gmail.com>
2013-12-15 13:34:19 -0500

    WordSearch completed

---
+++ 3/diep_timothy/26-wordsearchfinal/Driver.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+        WordSearch w = new WordSearch();
+	w.makeBoard(10);
+
+	w.fill();
+	   
+    }
+}
