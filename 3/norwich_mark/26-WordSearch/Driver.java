e86a3eafacaf6a47eddefdcea1fc188bb43c8d08
Mark Norwich <mna12@aol.com>
2013-12-16 03:00:24 -0500

    im tired

---
+++ 3/norwich_mark/26-WordSearch/Driver.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver {
+    public static void main(String[] args){
+        WordSearch w = new WordSearch();
+        System.out.println(w);
+	w.loadWords(wordList);
+	for (int i = 0; i < wordList.size(); i++){
+	    w.addWordRand(wordList.get(i));
+	}
+	System.out.println(w);
+	System.out.println(words.toString()):
+	    
+ }
+}
