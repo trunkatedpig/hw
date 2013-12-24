c37416e8041b168f19b185c960e285f26d9fba55
Richard Zhan <lilfirebolt@gmail.com>
2013-12-24 16:43:01 -0500

    Board kind of done

--- 2/zhan_richard/ChuChu/Chuchu.java
+++ 2/zhan_richard/ChuChu/Chuchu.java
@@ -10,19 +10,34 @@ import javax.imageio.*;
 public class Chuchu implements ActionListener{
     private JFrame frame;
     private JPanel canvas;
+    private Container pane;
     private JLabel test;
+    private boolean isGray=true;
     public void actionPerformed(ActionEvent e){
 	System.out.println(e);
     }
     public Chuchu(){
 	frame = new JFrame("CHUCHU ROCKETS!!!!");
-	frame.setLayout(new FlowLayout());
+	frame.setLayout(new GridLayout());
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	canvas = new JPanel();
+	canvas.setLayout(new GridLayout(12,12));
 	canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));
-	test = new JLabel("Testing this function");
-	canvas.add(test);
+	JPanel[] tiles = new JPanel[144];
+	int counter = 0;
+	for (JPanel j: tiles){
+	    j=new JPanel();
+	    if (isGray==true)
+		j.setBackground(Color.lightGray);
+	    else
+		j.setBackground(Color.darkGray);
+	    canvas.add(j);
+	    counter++;
+	    if (counter%12!=0)
+		isGray=!isGray;
+
+	}
 	frame.add(canvas);
 	frame.setSize(700,700);
 	frame.setVisible(true);
