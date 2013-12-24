05d2a6a9f76eb59abd20b3b73373d94aea8ca5af
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2013-12-24 14:54:34 -0500

    added empty Gui class

---
+++ 1/sapozhnikov_brian/FinalProject/Gui.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
+
+public class Gui implements ActionListener{
+    public static void main(String[] args){
+
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
