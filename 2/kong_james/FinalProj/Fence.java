437618307d4921497cffef3072dcb57caaae81fb
jameskong <jdetect1@gmail.com>
2013-12-25 01:46:52 -0500

    With Richard

--- 2/kong_james/FinalProj/Fence.java
+++ 2/kong_james/FinalProj/Fence.java
@@ -5,15 +5,27 @@ public class Fence{
     private String result;
     public String multiFence(int xcor, int ycor, int x, int y, String[] text){
 	result ="";
-	for (int i = 0; i <= (x+((xcor-1)*(x-1)))*(y+((ycor-1)*(y-1)));i++){
-	    if (i==0  ||  i==(x+((xcor-1)*(x-1)))  ||  i==(x+((xcor-1)*(x-1)))*(y+((ycor-1)*(y-1)))  )
-		result = result + "+";
-	    else
-		result = result + "-";
-	    if (i%(x+(xcor-1)*(x-1))==(x+(xcor-1)*(x-1))-1)
-		result=result+"\n";
+	int counter = 0;
+	for (int i = 0; i <= ycor*y;i++){
+	    for (int j = 0; j <= xcor*x; j++){
+		if ((i==0&&j==0) || (i==0&&j==xcor*x) || (i==ycor*y&&j==0) || (i==ycor*y&&j==xcor*x))
+		    result = result + "+";
+		else if (i%y ==0)
+		    result = result + "-";
+		else if (j%x ==0)
+		    result = result + "|";
+		else if (j%x ==1 && i%(y/2)==0){
+		    result = result + text[counter];
+		    j=j+text[counter].length()-1;
+		    counter++;
+		}
+		else
+		    result = result + " ";
+	    }
+	    result = result + "\n";
 	}
 	return result;
+	
     }
     public String singleFence(int x, int y,String text){
         result = "";
