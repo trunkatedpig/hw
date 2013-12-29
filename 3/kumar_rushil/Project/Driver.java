fa0ca127e9d2f9d90d837a78d9b70535032bee34
Rushil Kumar <rukumar333@gmail.com>
2013-12-28 23:01:34 -0500

    made a Weapon class and worked on Character

--- 3/kumar_rushil/Project/Driver.java
+++ 3/kumar_rushil/Project/Driver.java
@@ -5,7 +5,6 @@ public class Driver{
     public static void main (String[] args){
 	Character c = new Character("Rushil");
 	System.out.println(c);
-	c.addExp(5);
-	c.addExp(5);
+	c.meleeAttack();
     }
 }
