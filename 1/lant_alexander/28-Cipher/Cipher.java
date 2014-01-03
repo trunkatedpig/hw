2256c05ad6ff258fce79d58b68745219e025ae3c
Caspar Lant <casparlant@gmail.com>
2014-01-03 18:03:10 -0500

    Added Decipher - Caspar

--- 1/lant_alexander/28-Cipher/Cipher.java
+++ 1/lant_alexander/28-Cipher/Cipher.java
@@ -2,23 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Cipher{
+    
+    public ArrayList<String> wordList;
+
     public static void main(String[] args){
 
-	System.out.println((char) ('a' + 3));
-
-	if (args.length != 0){
-	Cipher c = new Cipher(args[0]);
+	if (args.length > 1){
+	    Cipher c = new Cipher(args[0], Integer.parseInt( args[1]));
 	}
-	else 
+	else
 	    System.out.println("No String given");
     }
     
-    public Cipher(String input){
-	//input = deCap(input);
+    public Cipher(String input, int num){
+       	//input = deCap(input);
 	String firstWord = getFirstWord(input);
 	String output = "";
-	System.out.println(Encrypt(input, 2));
-	
+	readWords("words.txt");
+	System.out.println(Encrypt(input, num));
+	System.out.println(Decrypt(input));
     }   
 
     public String getFirstWord(String input){
@@ -28,15 +30,31 @@ public class Cipher{
 	    for (int i = 0; input.charAt(i) != ' '; i++){
 		firstWord += input.charAt(i);
 		}
-	    System.out.println(firstWord); //for testing
 	}
 	else{
 	    for (int i = 0; i < input.length(); i ++){
 		firstWord += input.charAt(i);
 	    }
-	    System.out.println(firstWord); //for testing
 	}
 	return firstWord;
+    }
+
+    private void readWords(String filename){	
+	int longest = 0;
+
+	wordList = new ArrayList<String>();
+	try{
+	    Scanner sc = new Scanner(new File(filename));
+	    while(sc.hasNext()){
+		String s = sc.nextLine();
+		wordList.add(s);
+		if (s.length() > longest)
+		    longest = s.length();
+	    }
+	}catch (Exception e) {
+	    System.out.println(e);
+	    System.exit(0);
+	}
     }
 
     public String Encrypt(String input, int num){
@@ -56,9 +74,15 @@ public class Cipher{
     } 
     
     public String Decrypt(String input){
+	String firstWord = getFirstWord(input);
 	String output = "";
-
-	return output;
+	
+	for (int a = 1; a < 26; a++){
+	    if (wordList.contains(Encrypt(firstWord, a))){
+		return (Encrypt(firstWord, a));
+	    }
+	}
+	return "ERROR";
     }
     
 
