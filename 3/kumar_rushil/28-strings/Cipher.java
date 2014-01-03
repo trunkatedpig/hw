f2aa57fee15e10536f84f71fefe8a28b9a3b71e9
Rushil Kumar <rukumar333@gmail.com>
2014-01-03 01:08:03 -0500

    almost done

--- 3/kumar_rushil/28-strings/Cipher.java
+++ 3/kumar_rushil/28-strings/Cipher.java
@@ -1,9 +1,13 @@
 public class Cipher{
     public String runCipher(String s, int n){
 	String result = "";
+	int k = 0;
 	s = s.toLowerCase();
 	for (int i = 0; i < s.length(); i ++){
-	    if (s.charAt(i) + n < 'z'){
+	    if (s.charAt(i) + n <= 'z'){
+		result = result + (char)(s.charAt(i) + n);
+	    }
+	    if (s.charAt(i) + n > 'z'){
 		
 	    }
 	}
