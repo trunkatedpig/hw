91bf44cdfc1d23f11a6849067cd2fe2cb22d84b6
aliciachen <mail4aliciac@gmail.com>
2014-01-02 23:35:11 -0500

    added hw28

---
+++ 1/chen_alicia/28-Encode/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main (String [] args){
+	Cipher c = new Cipher();
+	System.out.println(c.encode("hello", 3));
+	System.out.println(c.encode("khoor", -3));
+	System.out.println(c.encode("abcxyz123123", 3));
+    }
+}
