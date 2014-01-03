c20891b66c6c7b95b16f132b7f4b1c83f8c04c45
Rushil Kumar <rukumar333@gmail.com>
2014-01-03 00:44:21 -0500

    not done

---
+++ 3/kumar_rushil/28-strings/Cipher.java
@@ -0,0 +1,12 @@
+public class Cipher{
+    public String runCipher(String s, int n){
+	String result = "";
+	s = s.toLowerCase();
+	for (int i = 0; i < s.length(); i ++){
+	    if (s.charAt(i) + n < 'z'){
+		
+	    }
+	}
+	return result;
+    }
+}
