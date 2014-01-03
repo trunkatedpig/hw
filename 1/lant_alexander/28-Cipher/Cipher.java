1cc9d94cc69074fb1ed775095486ad729eb8e8fe
Caspar Lant <casparlant@gmail.com>
2014-01-03 11:45:28 -0500

    - Caspar

--- 1/lant_alexander/28-Cipher/Cipher.java
+++ 1/lant_alexander/28-Cipher/Cipher.java
@@ -3,47 +3,63 @@ import java.util.*;
 
 public class Cipher{
     public static void main(String[] args){
-	try{
-	    Cipher C = new Cipher(args[0]);
-	}catch(Exception e){
-	    System.out.println(e + "\nNo String to decipher.");
-	}
-    }
 
-    public Cipher(){
-	System.out.println("Gimme a String!");
+	System.out.println((char) ('a' + 3));
+
+	if (args.length != 0){
+	Cipher c = new Cipher(args[0]);
+	}
+	else 
+	    System.out.println("No String given");
     }
     
     public Cipher(String input){
 	//input = deCap(input);
-	
+	String firstWord = getFirstWord(input);
 	String output = "";
-	String firstWord = "";
+	System.out.println(Encrypt(input, 2));
 	
-
-	for (int i = 0; input.charAt(i) != ' '; i++){
-	    firstWord += input.charAt(i);
-	}
-	System.out.println(firstWord);
-
-
-	for (int j = 0; j < 26; j++){
-	    for (int k= 0; k < input.length(); k++){
-		
-	    }
-	}
     }   
 
-    public String Encrypt(String input, String num){
-	String output = "";
-	for(int i = 0; i < input.length(); i++){
-	    if (input.charAt(i) + num <= 'z'){
-		output += input.charAt(i) + num;
+    public String getFirstWord(String input){
+	String firstWord = "";
+	
+	if (input.contains(" ")){
+	    for (int i = 0; input.charAt(i) != ' '; i++){
+		firstWord += input.charAt(i);
+		}
+	    System.out.println(firstWord); //for testing
 	}
-	    else{
-		output += input.charAt(i) - (26+num);
+	else{
+	    for (int i = 0; i < input.length(); i ++){
+		firstWord += input.charAt(i);
 	    }
+	    System.out.println(firstWord); //for testing
 	}
+	return firstWord;
+    }
+
+    public String Encrypt(String input, int num){
+      	String output = "";
+      	
+	for(int i = 0; i < input.length(); i++){
+	    if (input.charAt(i) + num > 'z' && input.charAt(i) >= 97 && input.charAt(i) <= 122){
+		output += ((char) (input.charAt(i) + (num - 26)));
+	    }
+	    else if(input.charAt(i) + num <= 'z' && input.charAt(i) >= 97 && input.charAt(i) <= 122){
+		output += ((char) (input.charAt(i) + num));
+	    }
+	    else
+		output += input.charAt(i);
+	}
+      	return output;
+    } 
+    
+    public String Decrypt(String input){
+	String output = "";
+
 	return output;
     }
+    
+
 }
