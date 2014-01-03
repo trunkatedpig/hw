94ab00a9bef098429b3b37f9db2ef9ff2424b6e0
Kemraj Sobhai <Kbsobhai@gmail.com>
2014-01-03 11:39:10 -0500

    updated Driver

--- 1/sobhai_kemraj/28-Cipher/Driver.java
+++ 1/sobhai_kemraj/28-Cipher/Driver.java
@@ -5,5 +5,10 @@ public class Driver{
 	public static void main (String[] args){
 		Cipher test = new Cipher();
 		System.out.println(test.encode("hello", 3));
+		System.out.println(test.encode("rot13", 3));
+		System.out.println(test.encode("123", 3));
+		System.out.println(test.encode("!@#$%^&*()", 3));
+		System.out.println(test.encode("bo0m", 3));
+		System.out.println(test.encode("evad3rs", 3));		
 	}
 }
