cd2a8e50e62c044e7a457dd1861509f139b1fdf0
Maria Kollaros <marakiimou7@gmail.com>
2014-01-06 18:33:39 -0500

    LESMIS

---
+++ 1/kollaros_maria/hw29/Cipher.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Cipher {
+
+    private double[] CorpusFreqs = new double[26];
+
+    public String encode(String orig, int offset) {
+        String result="";
+        char c;
+        String s = orig.toLowerCase();
+        for (int i=0;i<s.length();i++) {
+            c=s.charAt(i);
+            if (c>='a' && c<='z') {
+                c = (char) ( ( (c-'a'+offset)%26 ) +'a');
+            }
+            result = result + c;
+        }
+            return result;
+    }
+
+    private void buildCorpusFreq(String filename){
+	try {
+	    Scanner sc = new Scanner (new File(filename));
+	    while (sc.hasNext()) {
+		String s = sc.nextLine();
+		String p = s.toLowerCase();
+		for (int i=0;i<p.length();i++) {
+		    char c = p.charAt(i);
+		    int y = 0;
+		    if(c>= 'a' && c<='z'){
+			y = (int)(c - 'a');
+			CorpusFreqs[y] = CorpusFreqs[y] + 1;
+		    }
+		}
+	    }
+	}
+	    catch (Exception e) {}
+    }
+
+    public String toString(){
+	return Arrays.toString(CorpusFreqs);
+    }
+
+    public int Len(){
+	return CorpusFreqs.length;
+    }
+	
+	
+
+    public static void main(String[] args){
+        Cipher c = new Cipher();
+        System.out.println(c.encode("hello",3));
+	c.buildCorpusFreq("LesMis.txt");
+	System.out.println(c);
+	System.out.println(c.Len());
+	
+
+    }
+
+}
+
