1655fe3a7dacb6454897059251ed8c099daf6416
ericbm2 <ericbmorgenstern@gmail.com>
2014-01-06 22:37:18 -0500

    more project stuff

--- 2/morgenstern_eric/project/Driver.java
+++ 2/morgenstern_eric/project/Driver.java
@@ -3,5 +3,7 @@ import java.util.*;
 public class Driver {
     public static void main(String[] args){
 	//sets up the game, directs play
+	Player p = new Player();
+	System.out.println(p.roll());
     }
 }
