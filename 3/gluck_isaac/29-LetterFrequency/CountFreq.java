588d09e2c70ebd5d570d541bfdc6597aaeb53272
IsaacGluck <isaacbgluck@gmail.com>
2014-01-06 19:07:48 -0500

    added hw 29

--- 3/gluck_isaac/29-LetterFrequency/CountFreq.java
+++ 3/gluck_isaac/29-LetterFrequency/CountFreq.java
@@ -3,40 +3,54 @@ import java.io.*;
 
 public class CountFreq{
 	private double[] corpusFreqs = new double[26];
-	private double numLetters = 0;
+	private int total = 0;
+
+	public String encode(String s, int offset){
+		s = s.toLowerCase();
+		String result = "";
+
+		for (int i=0;i<s.length();i++){
+			char toEncode = s.charAt(i);
+			if (toEncode >= 97 && toEncode <= 122){
+				if (toEncode + offset <= 122)
+					result += (char)(toEncode + offset);
+				else{
+					while (offset > 43){
+						offset -= 26;
+					}
+					result += (char)(toEncode + offset - 26);
+				}
+			}
+			else
+				result += (char)toEncode;
+		}
+		return result;
+	}
 
 	public void buildCorpusFreqs(String filename){
-		double[] letterFreq = new double[26];
 		try {
         	Scanner scan = new Scanner (new File(filename));
         	while (scan.hasNext()) {
         	    String s = scan.nextLine().toLowerCase();
-
-        	    System.out.println(s);
-        	    System.out.println("" + scan.hasNext());
-        	    
         	    for (int i=0;i<s.length();i++) {
 					char c = s.charAt(i);
-					System.out.println(""+c);
-					if (c>='a' || c<='z'){
-						numLetters += 1.0;
-						letterFreq[c-'a'] += 1.0;
+					int tmp = 0;
+					if (c>='a' && c<='z'){
+						total += 1;
+						tmp = (int)(c-'a');
+						corpusFreqs[tmp] += 1.0;
 					}
         	    }
         	}
         	scan.close();
         	for (int i=0;i<26;i++){
-        		corpusFreqs[i] = letterFreq[i]/numLetters;
+        		corpusFreqs[i] = (corpusFreqs[i]/total)*100.0;
         	}
 		}catch(Exception e){}
 	}
 
 	public String toString(){
-		String result = "";
-		for(int i=0;i<26;i++){
-			result +=corpusFreqs[i] + ", ";
-		}
-		return result;
+		return Arrays.toString(corpusFreqs);
 	}
 
 	public static void main(String[] args){
