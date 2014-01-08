68247ba30e3ef50993723974541c1ff39e2dcc94
Vanessa <yu100997@gmail.com>
2014-01-08 08:43:03 -0500

    a

--- 1/yu_yan/28-caesar/Driver.java
+++ 1/yu_yan/28-caesar/Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[] args){
 	Caesar c = new Caesar();
-	c.encode("abcdefghijklmnoPQRSTUVWXYZ123!@#", 1);
+	c.encode("abcdefghijklmnoPQRSTUVWXYZ123!@#", 25);
     }
 }
