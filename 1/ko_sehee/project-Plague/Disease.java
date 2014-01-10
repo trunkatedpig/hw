d2eb88d54860fd261b57514f66eebfb1d1e9bc35
ChristinaKo <christinako822@gmail.com>
2014-01-10 08:42:24 -0500

    added Disease

--- 1/ko_sehee/project-Plague/Disease.java
+++ 1/ko_sehee/project-Plague/Disease.java
@@ -1,8 +1,15 @@
 public class Disease{
+    private double[][] continents= new double[7][6];
     private double infected,dead,chanceInfected,chanceDead;
     private int  DNAPoints;
     private String name;
     private  boolean nausea = false,coughing =false,rash=false,anemia=false,livestock=false,rodent=false,bird=false,insect=false,blood=false,air=false,water=false,coldR=false,heatR=false,drugR=false,GeneticH=false,GeneticR=false;
+    private int[] currentPop = {346000000,596000000,37000000,4216000000,1072000000};
+    private int[] airInteraction;
+    private int[] waterInteraction;
+    private int[] infected;
+    private int[] temp;
+				
     public Disease(String n){
 	name = n;
 	infected = 0;
@@ -106,17 +113,21 @@ public class Disease{
     }
     
 
-    
-
-    //only happens if player chooses to (2)WAIT AND ANALYZE
-    public String  turn(int choice) {
-	this.winYet();
-	if (Math.random() < chanceInfected) {
+    //Will be called when user first creates disease    
+    public void firstSpread(int cont){
+	//	continents[cont][0]= currentPop[cont];
+	//	continents[cont][1]=airInteraction[cont];
+	//	continents[cont][2]= waterInteraction[cont];
+	//	continents[cont][3]= infected[cont];
+	//	continents[cont][4]= .01;
+	//	continents[cont][5]= temp[cont];
+    }
+    public void spread(){
+if (Math.random() < chanceInfected) {
 	    infected = infected + 2 + (chanceInfected/2);
 	    System.out.println("Infectivity: " +( chanceInfected*100 )+"%");
 	    System.out.println("Infected: " +( infected*100 )+"%");
 	}
-	//your forgot the inequality sign here so i just put less than 
 	if (Math.random()< chanceDead) {
 	    dead = dead + 2 + (chanceDead/2);
 	    System.out.println("Severity: " +( chanceDead*100 )+"%");
@@ -124,6 +135,36 @@ public class Disease{
 	}
 
 
+
+	
+    }
+    //
+
+    public void upgrade(int choice){
+	if (choice == 1){
+	    //list choices
+	    //scanner
+	    //upgrade
+	}
+	    
+
+
+
+    //only happens if player chooses to (2)WAIT AND ANALYZE
+    public String  turn(int choice) {
+	this.winYet();
+	if (choice ==1){
+	    System.out.println("1 Upgrade Transmission");
+	    System.out.println("2 upgrade Abilities");
+	    System.out.println("3 Upgrade Symptoms");
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Choose one of above: ");
+	    int ans = sc.nextLine();
+	    upgrade(ans);
+
+	}
+
+	
 	if (Math.random() < chanceInfected+chanceDead) {
 	    DNAPoints = DNAPoints + 2;
 	    System.out.println("DNA Points: " + DNAPoints);
