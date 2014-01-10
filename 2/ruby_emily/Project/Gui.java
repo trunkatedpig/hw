8687b27774c453a76cec41543283cd3b18466f2b
Emily Ruby <emilyruby19@gmail.com>
2014-01-10 06:59:15 -0500

    workingthroughgui

--- 2/ruby_emily/Project/Gui.java
+++ 2/ruby_emily/Project/Gui.java
@@ -3,25 +3,55 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Gui extends JFrame implements ActionListener{
-    private JButton stopButton;
+    private JButton quitButton;
+    private JButton upperCase;
+    private JButton clear;
+    private JButton lowerCase;
     private JTextField textbox;
+
+    private JPanel panel;
     private Container pane;
     
     public Gui(){
-	stopButton=new JButton("Stop");
-	this.add(stopButton);
-       	stopButton.addActionListener(this);
-	textbox=new JTextField(5);
+	clear=new JButton("Clear");
+	upperCase=new JButton("Upper Case");
+	lowerCase=new JButton("Lower Case");
+	quitButton=new JButton("Quit");
+
+	clear.addActionListener(this);
+	lowerCase.addActionListener(this);
+	upperCase.addActionListener(this);
+       	quitButton.addActionListener(this);
+
+	textbox=new JTextField(15);
 	this.add(textbox);
+
+	panel=new JPanel (new BorderLayout());
+
 	pane=this.getContentPane();
 	pane.setLayout(new FlowLayout());
 	pane.add(textbox);
-	pane.add(stopButton);
+	pane.add(quitButton);
+	pane.add(upperCase);
+	pane.add(lowerCase);
+	pane.add(clear);
 
     }
      public void actionPerformed(ActionEvent e){
-	if (e.getSource()==stopButton){
+	if (e.getSource()==quitButton){
 	    System.exit(0);
+	}
+	if (e.getSource()==upperCase){
+	    String s=textbox.getText();
+	    textbox.setText(s.toUpperCase());
+	}
+	if (e.getSource()==lowerCase){
+	    String s=textbox.getText();
+	    textbox.setText(s.toLowerCase());
+	}
+	if (e.getSource()==clear){
+	    String s="";
+	    textbox.setText(s);
 	}
 	}
 
