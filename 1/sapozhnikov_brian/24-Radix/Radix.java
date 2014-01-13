5c025134db29f1c4951f037086c1945dea914d0f
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-01-13 08:39:49 -0500

    added bubble sort

--- 1/sapozhnikov_brian/24-Radix/Radix.java
+++ 1/sapozhnikov_brian/24-Radix/Radix.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Radix{
+    private long bubbleComps = 0;
+    private long bubbleSwaps = 0;
+
     public int[] sort(int[] A){
         ArrayList[] buckets = new ArrayList[10];
         int n = 0;
@@ -21,6 +24,23 @@ public class Radix{
 	    n = n + 1;
         }
         return A;
+    }
+
+    public int[] bubble(int[] A){
+	for (int i =0; i<A.length-1; i++){
+	    for (int n =0; n<A.length-1; n++){
+		bubbleComps ++;
+		if (A[n] > A[n+1]){
+		    bubbleSwaps ++;
+		    int tmp = A[n];
+		    A[n] = A[n+1];
+		    A[n+1] = tmp;
+		}
+	    }
+	}
+	System.out.println("Comparisons: " + bubbleComps);
+	System.out.println("Swaps: " + bubbleSwaps);
+	return A;
     }
 
      // public String toString(int[] A){
