f9eb751408c90b92cafc6606589a1884fc7f6504
James Chen <thejamesschen@gmail.com>
2014-01-13 19:35:15 -0500

    hw 30

---
+++ 2/chen_james/30-Sorts/Driver.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+    public static void main (String args[]){
+	long start, end;
+
+	RadixSort a = new RadixSort(10,4);
+	System.out.println(a);
+	a.bsort(a);
+	System.out.println(a);
+
+    }
+}
