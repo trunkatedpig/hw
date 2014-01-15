c65bdbcc34b3673c02015bb9ef79b3a27c370853
fawnwong <fawn.wong@live.com>
2014-01-14 23:12:58 -0500

    added select sort

--- 2/wong_fawn/30-sort/Sort.java
+++ 2/wong_fawn/30-sort/Sort.java
@@ -2,20 +2,105 @@ import java.io.*;
 import java.util.*;
 
 public class Sort {
-    int[] a = new int[200000];
-    int len = a.length;
-    Random r = new Random();
 
-    public Sort() {
-	for (int i = 0; i < len; i++) {
-	    a[i] = r.nextInt(1000);
-	}
+ 	public int[] createArray(int maxDigit, int numItems) {
+ 		int[] a = new int[numItems];
+ 		for (int i = 0; i < numItems; i++) {
+		    Random r = new Random();
+		    a[i] = r.nextInt((int)(Math.pow(10, maxDigit)));
+		}
+		return a;
+ 	}
+
+    public int[] bubbleSort(int[] a) {
+    	int comps = 0, swaps = 0;
+    	System.out.println("\nBUBBLE SORT");
+    	int len = a.length;		
+    	long start, end;
+		start = System.currentTimeMillis();
+		for (int y = len; y > 0; y--) {
+		    for (int i = 1; i < y; i++) {
+			int x = a[i];
+			comps ++;
+			if (x < a[i-1]) {
+			    a[i] = a[i-1];
+			    a[i-1] = x;
+			    swaps ++;
+			}
+		    }
+		}
+		end = System.currentTimeMillis();
+		System.out.println("time: " + (end - start));
+		System.out.println("comparisons: " + comps);
+		System.out.println("swaps: " + swaps);
+
+		return a;
     }
 
-    public void selectSort() {
-	int tempmin = a[0];
-	for (int i = 1; i < len; i++) {
-	    for (int x = i +
-	}
+    public int[] bubbleSortOpt(int[] a) {
+    	int comps = 0, swaps = 0;
+    	System.out.println("\nBUBBLE SORT OPTIMIZED");
+		Boolean done = false;
+		long start, end;
+		int len = a.length;
+		start = System.currentTimeMillis();
+		for (int y = len; y > 0 && !done; y--) {
+			done = true;
+		    for (int i = 1; i < y; i++) {
+			int x = a[i];
+			comps ++;
+				if (x < a[i-1]) {
+					done = false;
+				    a[i] = a[i-1];
+				    a[i-1] = x;
+				    swaps ++;
+				}
+		    }
+		}
+		end = System.currentTimeMillis();
+		System.out.println("time: " + (end - start));
+		System.out.println("comparisons: " + comps);
+		System.out.println("swaps: " + swaps);
+		return a;
     }
+
+    public int[] selectSort(int[] a) {
+    	int comps = 0, swaps = 0;
+    	System.out.println("\nSELECT SORT");
+		int len = a.length;
+		long start, end;	
+		start = System.currentTimeMillis();	
+		for (int i = 1; i < len - 1; i++) {
+			int tempMin = a[i];
+			int ind = i;
+		    for (int x = i + 1; x < len - 2; x++) {
+		    	comps++;
+		    	if (a[x] < tempMin) {
+		    		tempMin = a[x];
+		    		ind = x;
+		    		swaps++;
+		    	}
+		    }
+		    a[ind] = a[i];
+		    a[i] = tempMin;
+		}
+		end = System.currentTimeMillis();
+		System.out.println("time: " + (end - start));
+		System.out.println("comparisons: " + comps);
+		System.out.println("swaps: " + swaps);		
+		return a;
+    }
+
+    public int[] insertSort(int[] a) {
+    	return a;
+    }
+
+    public void printArray(int[] a) {
+    	String s = "";
+    	for (int i = 0; i < a.length; i++) {
+    		s = s + a[i] + ", ";
+    	}
+    	System.out.println(s);
+    }
+    
 }
