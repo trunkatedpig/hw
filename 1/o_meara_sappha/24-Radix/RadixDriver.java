b9ba769ee0fa208e965e1f83ada98ce0ce9c7576
sapphacs13 <sappha.omeara@gmail.com>
2014-01-16 06:50:20 -0500

    Update RadixDriver.java

--- 1/o_meara_sappha/24-Radix/RadixDriver.java
+++ 1/o_meara_sappha/24-Radix/RadixDriver.java
@@ -1,39 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-public class RadixDriver{
-    //worked with Brian and Emile
+public class Driver{
     public static void main(String[] args){
-        int[] A = new int[100000];
+        int[] A = new int[1000000];
         int[] B = new int[1000000];
+	int[] C = new int[1000000];
+	int[] D = new int[1000000];
         Random r = new Random();
         for (int i = 0; i < A.length; i ++){
             A[i] = r.nextInt(10000000);
             B[i] = r.nextInt(10000000);
+	    C[i] = r.nextInt(10000000);
+	    D[i] = r.nextInt(10000000);
         }
 
         Radix R = new Radix();
 
         long start = System.currentTimeMillis();
-                
-        //A = R.sort(A);
-        A = R.bubble(A);
-
+        A = R.sort(A);
         long t = System.currentTimeMillis();
-        //System.out.println("Radix sort: " + (t - start));
-        System.out.println("Bubble Sort: " + (t - start));
-        
-        // System.out.println(Arrays.toString(A));
-        
-        // System.out.println(R.toString(R.sort(A)));
+        System.out.println("Radix sort: " + (t - start));
 
         start = System.currentTimeMillis();
-        
-        //Arrays.sort(B);
-
+        B = R.bubble(B);
         t = System.currentTimeMillis();
-        //System.out.println("Built-in sort: " + (t - start));
-        // System.out.println(Arrays.toString(B));
+        System.out.println("Bubble sort: " + (t - start));
 
+	start = System.currentTimeMillis();
+        C = R.selection(C);
+        t = System.currentTimeMillis();
+        System.out.println("Selection sort: " + (t - start));
+	
+	start = System.currentTimeMillis();
+        D = R.insertion(D);
+        t = System.currentTimeMillis();
+        System.out.println("Insertion sort: " + (t - start));
+	
     }
 }
