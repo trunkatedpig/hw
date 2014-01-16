91a0e90b1e546720681e7842b74245a9d697f285
Anna213 <aruta213@gmail.com>
2014-01-15 23:37:20 -0500

    Created Insertion and Selection Sorters

--- 3/ruta_anna/30-sorters/Driver.java
+++ 3/ruta_anna/30-sorters/Driver.java
@@ -31,33 +31,67 @@ public class Driver {
         System.out.println("Number of Swaps: " + t.getSwap());
         System.out.println();
 
+        Sorter u = new Sorter(3,10);
+
+        System.out.println("Original Int Array:");
+		System.out.println(u);
+		u.selection(u.getnums());
+		System.out.println("Selection-Sorted Int Array:");
+		System.out.println(u);
+		System.out.println("Number of Comparisons: " + u.getComp());
+        System.out.println("Number of Swaps: " + u.getSwap());
+        System.out.println();
+
+        Sorter v = new Sorter(3,10);
+
+		System.out.println("Original Int Array:");
+		System.out.println(v);
+		v.insertion(v.getnums());
+		System.out.println("Insertion-Sorted Int Array:");
+		System.out.println(v);
+		System.out.println("Number of Comparisons: " + v.getComp());
+        System.out.println("Number of Swaps: " + v.getSwap());
+        System.out.println();
+
         System.out.println("PART 2: Testing Speed of Sorters");
+        System.out.println("Each Sorter Sorts a 100,000 3-digit numbers");
         System.out.println();
 
         long start, timer;
 
-        Sorter u = new Sorter(3,100000);
+        Sorter a = new Sorter(3,100000);
         start = System.currentTimeMillis();
-		u.radix();
+		a.radix();
 		timer = System.currentTimeMillis()-start;
 		System.out.println("Time for my Radix Sort: " + timer);
 
-		 Sorter v = new Sorter(3,100000);
-		 start = System.currentTimeMillis();
-		 v.bubble(v.getnums());
-		 timer = System.currentTimeMillis()-start;
-		 System.out.println("Time for my Bubble Sort: " + timer);
+		Sorter b = new Sorter(3,100000);
+		start = System.currentTimeMillis();
+		b.bubble(b.getnums());
+		timer = System.currentTimeMillis()-start;
+		System.out.println("Time for my Bubble Sort: " + timer);
 
+		Sorter c = new Sorter(3,100000);
+		start = System.currentTimeMillis();
+		c.selection(c.getnums());
+		timer = System.currentTimeMillis()-start;
+		System.out.println("Time for my Selection Sort: " + timer);
+
+		Sorter d = new Sorter(3,100000);
+		start = System.currentTimeMillis();
+		d.insertion(d.getnums());
+		timer = System.currentTimeMillis()-start;
+		System.out.println("Time for my Insertion Sort: " + timer);
 
 		Random rand = new Random();
-		int[] a = new int[100000];
+		int[] e = new int[100000];
 		int range = 1;
 		for (int j = 0; j<3; j++)
 		    range = range * 10;
 		for (int i = 0; i<100000; i++)
-         	a[i] = rand.nextInt(range);
+         	e[i] = rand.nextInt(range);
         start = System.currentTimeMillis();
-		Arrays.sort(a);
+		Arrays.sort(e);
 		timer = System.currentTimeMillis()-start;
 		System.out.println("Time for built-in java Sorting: " + timer);
 
