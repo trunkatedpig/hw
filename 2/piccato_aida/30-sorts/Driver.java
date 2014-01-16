48ba0430e4b8c019d10dc995af2170d729037e03
Aida Piccato <aidapiccato@gmail.com>
2014-01-16 00:52:15 -0500

    Added selection sort and part of insertion sort

--- 2/piccato_aida/30-sorts/Driver.java
+++ 2/piccato_aida/30-sorts/Driver.java
@@ -4,24 +4,35 @@ import java.util.*;
 public class Driver {
     public static void main(String[] args) {
 	Sorts sort = new Sorts(100000);
-	//	System.out.println(bs);
+
+	//       System.out.println(sort);
+
 	long start = System.currentTimeMillis();
 	
-	sort.bsort(sort.getNums());
+	sort.sSort(sort.getNums());
 	
 	long end = System.currentTimeMillis();
 
-      	System.out.println("BubbleSort time: " + (end - start));
-	//System.out.println(bs);
+      	System.out.println("Selection Sort time: " + (end - start));
 
+	//	System.out.println(sort);
 	
 	start = System.currentTimeMillis();
 
-	sort.rsort();
+	sort.bSort(sort.getNums());
 
 	end = System.currentTimeMillis();
 	
-	System.out.println("Radix time: " + (end - start)); 
+	System.out.println("Bubble Sort time: " + (end - start)); 
 	
+
+       	start = System.currentTimeMillis();
+
+	sort.rSort(sort.getNums());
+
+	end = System.currentTimeMillis();
+	
+	System.out.println("Radix Sort time: " + (end - start)); 
+
     }
 }
