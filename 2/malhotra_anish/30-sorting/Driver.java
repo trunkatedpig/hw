173679ca3436bcb4bf7f683eac50bacc5784b40f
Anish Malhotra <anishmalhotra97@gmail.com>
2014-01-28 12:18:38 -0500

    for some reason these ones were gone?

---
+++ 2/malhotra_anish/30-sorting/Driver.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        int length;
+        if (args.length == 1) {
+            length = Integer.parseInt(args[0]);
+        }
+        else {
+            length = 100;
+        }
+        Sorts s = new Sorts(length);
+
+        System.out.println("--- Radix Sort ---");
+        s.radixSort();
+
+        System.out.println("\n--- Bubble Sort ---");
+        s.bubbleSort();
+
+        System.out.println("\n--- Insertion Sort ---");
+        s.insertionSort();
+
+        System.out.println("\n--- Selection Sort ---");
+        s.selectionSort();
+    }
+}
